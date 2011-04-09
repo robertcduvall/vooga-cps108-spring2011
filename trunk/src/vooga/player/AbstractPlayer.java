9d44a596070629100cf5439aef922600b8acd096
kevintoaster@gmail.com <kevintoaster@gmail.com@5a3452c7-76ba-512d-50cd-69e64edf86dc>
2011-04-09 20:12:57 +0000

    abstractPlayer class added

--- trunk/src/vooga/player/AbstractPlayer.java
+++ trunk/src/vooga/player/AbstractPlayer.java
@@ -2,5 +2,36 @@ package vooga.player;
 
 public abstract class AbstractPlayer
 {
-
+	/**
+	 * @author Kevin Tao and Andrea Scripa
+	 */
+	
+	/*
+	 * If needed, one extension is to add a history of commands and events
+	 */
+	
+	int playerId;
+	long timeOfLastChange;
+	long timeOfLastChangeQuery;
+	
+	public abstract int compareTo(AbstractPlayer p);
+	
+	public abstract AbstractPlayer clone();
+	
+	public abstract boolean equals(AbstractPlayer p);
+	
+	public boolean hasChanged(){
+		
+		if(timeOfLastChange > timeOfLastChangeQuery){
+			timeOfLastChangeQuery = System.nanoTime();
+			return true;
+		}else{
+			timeOfLastChangeQuery = System.nanoTime();
+			return false;
+		}
+	}
+	public void changeOccurred(){
+		timeOfLastChange = System.nanoTime();
+	}
+	
 }
