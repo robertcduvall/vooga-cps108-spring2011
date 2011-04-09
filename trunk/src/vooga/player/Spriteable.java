3ceba0dd818e2681d1083406c23d8233309dd4bb
ascripa@gmail.com <ascripa@gmail.com@5a3452c7-76ba-512d-50cd-69e64edf86dc>
2011-04-09 21:45:01 +0000

    not sure what i did...

---
+++ trunk/src/vooga/player/Spriteable.java
@@ -0,0 +1,15 @@
+package vooga.player;
+
+//import java.awt.image.BufferedImage;
+import com.golden.gamedev.object.Sprite;
+
+public interface Spriteable
+{
+//    public void setIcon(BufferedImage i);
+//    
+//    public double getX(Sprite s);
+//    
+//    public double getY(Sprite s);
+    
+    public Sprite getSprite();
+}
