ec5bcc39240488581a6372b7fedc6d1c645e629b
kevintoaster <kevintoaster@gmail.com>
2011-04-09 21:25:00 +0000

    added comment

--- src/vooga/player/AIPlayer.java
+++ src/vooga/player/AIPlayer.java
@@ -6,6 +6,7 @@ package vooga.player;
 /**
  * @author Kevin Tao and Andrea Scripa
  *
+ * We will create an all-events listener, to feed info to the AIPlayer.
  */
 public abstract class AIPlayer extends AbstractPlayer{
     //Need a way of getting information about what the opponent has done.
