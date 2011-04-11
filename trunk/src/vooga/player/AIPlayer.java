5847cba6c5d92c210dfb621ef71f282072d4045e
ascripa@gmail.com <ascripa@gmail.com@5a3452c7-76ba-512d-50cd-69e64edf86dc>
2011-04-11 21:07:24 +0000

    Took out some things that were no longer needed/breaking the project

--- trunk/src/vooga/player/AIPlayer.java
+++ trunk/src/vooga/player/AIPlayer.java
@@ -4,10 +4,12 @@
 package vooga.player;
 
 /**
- * @author Kevin Tao and Andrea Scripa
- *
- * We will create an all-events listener, to feed info to the AIPlayer.
+ * @author Kevin Tao and Andrea Scripa We will create an all-events listener, to
+ *         feed info to the AIPlayer.
  */
-public abstract class AIPlayer extends AbstractPlayer{
+public abstract class AIPlayer extends AbstractPlayer
+{
     //Need a way of getting information about what the opponent has done.
+    //Poll for all events in the game,  Or the developer can refine to only poll for 
+    //certain ones.
 }
