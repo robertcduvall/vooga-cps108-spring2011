14f0e5d48b815e60f8e7c0652d5cba04e19309a8
CJAMES1224 <CJAMES1224@5a3452c7-76ba-512d-50cd-69e64edf86dc>
2011-04-11 18:19:45 +0000

    added quick example that may or may not work and needs tweaking. but it has fun with cats!

---
+++ src/vooga/replay/examples/catroll/CatReplay.java
@@ -0,0 +1,15 @@
+package vooga.replay.examples.catroll;
+
+
+import vooga.replay.Replay;
+import vooga.replay.StateTable;
+
+import com.golden.gamedev.GameEngine;
+
+public class CatReplay extends Replay {
+
+	public CatReplay(GameEngine parent, StateTable table) {
+		super(parent, table);
+	}
+	
+}
---
+++ src/vooga/replay/examples/catroll/CatRollGame.java
@@ -0,0 +1,40 @@
+package vooga.replay.examples.catroll;
+import java.awt.Dimension;
+
+import vooga.replay.StateTableFileManager;
+
+
+import com.golden.gamedev.GameEngine;
+import com.golden.gamedev.GameLoader;
+import com.golden.gamedev.GameObject;
+
+
+public class CatRollGame extends GameEngine{
+
+	
+	private StateTableFileManager stfm = new StateTableFileManager();
+	
+	@Override
+	public GameObject getGame(int id) {
+		switch (id){
+		case 0 : return new MainMenu(this);
+		case 1 : return new Level1(this);
+		case 2 : try {
+				return new CatReplay(this, stfm.returnStateTableFromFile("st"));
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		default: return new MainMenu(this);
+		}
+	}
+	
+	
+	public static void main(String[] args) {
+		GameLoader game = new GameLoader();
+		CatRollGame cat = new CatRollGame();
+		game.setup(cat, new Dimension(
+				800,600), false);
+		game.start();
+	}
+
+}
