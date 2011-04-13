6fe87342a8ebd6dbbcb02506984cfd45eb40fa58
alexanderwdaniel@gmail.com <alexanderwdaniel@gmail.com@5a3452c7-76ba-512d-50cd-69e64edf86dc>
2011-04-13 18:27:34 +0000

    setup added

--- trunk/src/games/asteroids/Asteroids.java
+++ trunk/src/games/asteroids/Asteroids.java
@@ -2,18 +2,54 @@ package games.asteroids;
 
 import java.awt.Graphics2D;
 
+import com.golden.gamedev.object.Background;
 import com.golden.gamedev.object.PlayField;
+import com.golden.gamedev.object.Sprite;
+import com.golden.gamedev.object.SpriteGroup;
+import com.golden.gamedev.object.collision.BasicCollisionGroup;
 
 import vooga.core.*;
 import vooga.core.event.IEventHandler;
 
 public class Asteroids extends VoogaGame {
 	private PlayField myPlayfield;
+    private Background mBackground;
+    private SpriteGroup myShip;
+    private SpriteGroup myAsteriods;
+    private BasicCollisionGroup playerMissileToAsteroid, playerToAsteroid;
+    private int score;
+
+    private void setup()
+    {
+        myPlayfield = new PlayField(); // Background from resources
+        playerMissileToAsteroid = new BasicCollisionGroup()
+        {
+            @Override
+            public void collided(Sprite s1, Sprite s2)
+            {
+                s1.setActive(false);
+                s2.setActive(false);
+                score++;
+            }
+        };
+        playerToAsteroid = new BasicCollisionGroup()
+        {
+
+            @Override
+            public void collided(Sprite s1, Sprite s2)
+            {
+                s1.setActive(false);
+                s2.setActive(false);
+            }
+        };
+
+    }
+	
 	
 	@Override
 	public void initResources() {
 		//TODO: Setup team fill this in.
-		setup();
+		this.setup();
 		//fire Event to start menu
 		super.fireEvent(this, "start.menu");
 		//fire event to start level
@@ -28,11 +64,6 @@ public class Asteroids extends VoogaGame {
 
 	private void onLevelComplete() {
 		super.fireEvent(this, "level.change");
-	}
-	
-	private void setup() {
-		// TODO setup team will fill this in
-		
 	}
 
 	@Override
