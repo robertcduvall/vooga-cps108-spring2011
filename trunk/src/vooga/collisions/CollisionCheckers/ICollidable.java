bb11d0eb2307d8d1770c889f8243f04b1e9039ec
jgenk90 <jgenk90@5a3452c7-76ba-512d-50cd-69e64edf86dc>
2011-04-15 17:15:32 +0000

    collidable updated

--- trunk/src/vooga/collisions/CollisionCheckers/ICollidable.java
+++ trunk/src/vooga/collisions/CollisionCheckers/ICollidable.java
@@ -4,11 +4,18 @@ import java.awt.geom.Line2D;
 import java.awt.geom.Line2D.Double;
 import java.awt.image.BufferedImage;
 import java.util.List;
+import vooga.collisions.shapes.collisionShapes.ICollisionShape;
 
 public interface ICollidable
 {
     
-    Iterable<Line2D> getCollisionLines();
-    
+    /**
+     * Returns default {@linkplain #defaultCollisionShape collision shape}, can
+     * be used along with collision manager.
+     */
+    public abstract ICollisionShape getCollisionShape ();
+
+
+    public abstract void setCollisionShape (ICollisionShape cs);    
     
 }
