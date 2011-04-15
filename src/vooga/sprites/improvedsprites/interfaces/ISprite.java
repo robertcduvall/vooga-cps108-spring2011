9dc10af9852fcd390350e1919704602389d756c0
jgenk90 <jgenk90@5a3452c7-76ba-512d-50cd-69e64edf86dc>
2011-04-15 16:26:06 +0000

    ISprite simplified

--- src/vooga/sprites/improvedsprites/interfaces/ISprite.java
+++ src/vooga/sprites/improvedsprites/interfaces/ISprite.java
@@ -2,20 +2,14 @@ package vooga.sprites.improvedsprites.interfaces;
 
 import vooga.collisions.shapes.regularShapes.Polygon;
 
+import vooga.collisions.shapes.collisionShapes.ICollisionShape;
 import com.golden.gamedev.object.collision.CollisionShape;
 
 
 public interface ISprite 
 {
 
-    /**
-     * Returns default {@linkplain #defaultCollisionShape collision shape}, can
-     * be used along with collision manager.
-     */
-    public abstract Polygon getCollisionShape ();
 
-
-    public abstract void setCollisionShape (Polygon cs);
     
     /**
      *  updates the sprite
