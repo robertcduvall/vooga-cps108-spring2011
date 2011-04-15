27ec441022564478635792a70b4f50216352844d
wbrown0389 <wbrown0389@5a3452c7-76ba-512d-50cd-69e64edf86dc>
2011-04-15 05:34:27 +0000

    Working on XML importing. Currently having an issue with importing objects that have references to an object of the same type.

--- trunk/src/vooga/levels/util/MappingBuilder.java
+++ trunk/src/vooga/levels/util/MappingBuilder.java
@@ -1,34 +1,32 @@
-package vooga.levels.util;
+package levels.util;
 
 import java.io.*;
 
 //import org.exolab.castor.mapping.MappingException;
 //import org.exolab.castor.tools.MappingTool;
 //import org.exolab.castor.xml.XMLContext;
+//
+//import com.golden.gamedev.object.Background;
+//
+//import sprites.Order;
+//import sprites.TestSprite;
 
 public class MappingBuilder {
 
 	public MappingBuilder() {
-//		try {
+		try {
 //			XMLContext context = new XMLContext(); 
 //			MappingTool tool = context.createMappingTool(); 
 //			tool.addClass(Order.class); 
 //			OutputStream file = new FileOutputStream("test.xml" ); 
 //			Writer writer = new OutputStreamWriter(file); 
 //			tool.write(writer); 
-//			
-////			MappingTool mt = new MappingTool();
-////			mt.setInternalContext(new org.castor.xml.BackwardCompatibilityContext());
-////			mt.addClass(Order.class);
-////			//mt.addClass(Bomb.class);
-////			OutputStream file = new FileOutputStream("test.xml");
-////			Writer writer = new OutputStreamWriter(file);
-////			mt.write(writer);
-//		} catch (MappingException e) {
-//			e.printStackTrace();
-//		} catch (FileNotFoundException e) {
-//			e.printStackTrace();
-//		}
+			
+		} catch (MappingException e) {
+			e.printStackTrace();
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
 	}
 
 }
