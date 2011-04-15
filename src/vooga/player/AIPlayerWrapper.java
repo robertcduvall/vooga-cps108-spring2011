83c8fa4d0c0cb13cca272dd82a346eef0eb395d3
ascripa <ascripa@gmail.com>
2011-04-15 04:27:19 +0000

    Player is finished =)

--- src/vooga/player/AIPlayerWrapper.java
+++ src/vooga/player/AIPlayerWrapper.java
@@ -6,13 +6,25 @@ package vooga.player;
 /**
  * @author Andrea Scripa
  */
-public abstract class AIPlayerWrapper extends AbstractPlayerWrapper 
+public abstract class AIPlayerWrapper extends AbstractPlayerWrapper
 {
+    /**
+     * AIPlayer's constructor.
+     */
     public AIPlayerWrapper (Player p)
     {
         super(p);
+        p.storeWrapper(this);
+        type = Player.AI_PLAYER;
     }
     
+    /**
+     * AIPlayer's input listeners.  These should generally be to certain changes in the 
+     * state of the game.  These listeners should parse the input and pass it to
+     * calculateAIResponse, which will likely call the AI engine when it is made.
+     * The response should come back in the form of a string (the Player's command), which
+     * gets passed to Player's updateEntities method.
+     */
     public abstract void addInputListeners();
     
     public abstract void calculateAIResponse(String event);
--- src/vooga/player/NetworkPlayerWrapper.java
+++ src/vooga/player/NetworkPlayerWrapper.java
@@ -6,11 +6,20 @@ package vooga.player;
  */
 public abstract class NetworkPlayerWrapper extends AbstractPlayerWrapper
 {
+    /**
+     * NetworkPlayer's constructor.
+     */
     public NetworkPlayerWrapper (Player p)
     {
         super(p);
+        p.storeWrapper(this);
+        type = Player.NETWORK_PLAYER;
     }
 
-    //Decodes the network packets
+    /**
+     * NetworkPlayer's input listeners.  These should be connected to the network engine
+     * and should decode the packets it listens for.  This input gets parsed into a 
+     * command (String) and sent to the Player's updateEntities method.
+     */
     public abstract void addInputListeners();
 }
