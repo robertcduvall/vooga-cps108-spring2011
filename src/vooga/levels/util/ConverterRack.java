9f8b8d921e19ce6bc745bb35fc897f5e51655a76
sterling.dorminey <sterling.dorminey@gmail.com>
2011-04-16 21:38:11 +0000

    Initial import of level parser.

---
+++ src/vooga/levels/util/ConverterRack.java
@@ -0,0 +1,65 @@
+package vooga.levels.util;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * An extendible class that allows conversion of strings to specified types.
+ * @author Sterling Dorminey
+ *
+ */
+public class ConverterRack {
+	private Map<Class<?>, Converter<?>> conversionMap;
+	
+	/* Some built-in converters for common operations. */
+	private class StringConverter extends Converter<String> {
+		@Override
+		public String convert(String input) {
+			return input;
+		}
+	}
+	
+	private class IntegerConverter extends Converter<Integer> {
+		@Override
+		public Integer convert(String input) {
+			return Integer.parseInt(input);
+		}
+	}
+	
+	private class BooleanConverter extends Converter<Boolean> {
+		@Override
+		public Boolean convert(String input) {
+			return input.toLowerCase().equals("true");
+		}
+	}
+	
+	private class DoubleConverter extends Converter<Double> {
+		@Override
+		public Double convert(String input) {
+			return Double.parseDouble(input);
+		}
+	}
+	
+	public ConverterRack() {
+		conversionMap = new HashMap<Class<?>, Converter<?>>();
+		
+		addConverter(String.class, new StringConverter());
+		addConverter(Integer.class, new IntegerConverter());
+		addConverter(Boolean.class, new BooleanConverter());
+		addConverter(Double.class, new DoubleConverter());
+	}
+	
+	public Object convert(Class<?> target, String input) {
+		if(!conversionMap.containsKey(target)) {
+			throw new RuntimeException("Unable to convert");
+		}
+		
+		Converter<?> converter = conversionMap.get(target);
+		
+		return converter.convert(input);
+	}
+	
+	public void addConverter(Class<?> target, Converter<?> converter) {
+		conversionMap.put(target, converter);
+	}
+}
---
+++ src/vooga/levels/util/tags/SpriteTag.java
@@ -0,0 +1,46 @@
+package vooga.levels.util.tags;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.w3c.dom.Element;
+import org.w3c.dom.NodeList;
+
+import vooga.levels.util.LevelParser;
+import vooga.levels.util.SpriteConstructor;
+import vooga.resources.xmlparser.Parser;
+import vooga.resources.xmlparser.XMLTag;
+
+public class SpriteTag extends XMLTag {
+	private static final String GROUP = "group";
+	private static final String CLASS = "class";
+	private static final String NAME = "name";
+	private static final String SPRITE = "sprite";
+	
+	private LevelParser parser;
+	
+	@Override
+	public String getTagName() {
+		return SPRITE;
+	}
+
+	public SpriteTag(LevelParser parser) {
+		this.parser = parser;
+	}
+	
+	@Override
+	public void parse(Parser context, Element xmlElement) {
+		String className = xmlElement.getAttribute(CLASS);
+		String name = xmlElement.getAttribute(NAME);
+		String spriteGroup = xmlElement.getAttribute(GROUP);
+		
+		// Get the list of children and use them for constructor arguments.
+		NodeList children = xmlElement.getChildNodes();
+		List<String> args = new ArrayList<String>(children.getLength());
+		for(int i = 0; i < children.getLength(); i++) {
+			args.add(getValue((Element) children.item(i)));
+		}
+		
+		parser.addSprite(name, new SpriteConstructor(parser.getConverterRack(), className, args));
+	}
+}
