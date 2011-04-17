9a14d71e31d487e2795f0514cdcbe84e2b2762d6
sterling.dorminey <sterling.dorminey@gmail.com>
2011-04-17 18:21:24 +0000

    Added deferred constructors for the pool.

--- src/vooga/levels/util/tags/InstanceTag.java
+++ src/vooga/levels/util/tags/InstanceTag.java
@@ -7,41 +7,40 @@ import org.w3c.dom.Element;
 import org.w3c.dom.NodeList;
 
 import vooga.levels.util.LevelParser;
-import vooga.levels.util.SpriteConstructor;
+import vooga.levels.util.PoolDeferredConstructor;
 import vooga.resources.xmlparser.Parser;
 import vooga.resources.xmlparser.XMLTag;
 
-public class SpriteTag extends XMLTag {
-	private static final String GROUP = "group";
-	private static final String CLASS = "class";
-	private static final String NAME = "name";
-	private static final String SPRITE = "sprite";
+public class InstanceTag extends XMLTag {
+	private static final String TAG_NAME = "instance";
+	private static final String TYPE_ATTR = "type";
 	
 	private LevelParser parser;
 	
 	@Override
 	public String getTagName() {
-		return SPRITE;
-	}
-
-	public SpriteTag(LevelParser parser) {
-		this.parser = parser;
+		return TAG_NAME;
 	}
 	
+	public InstanceTag(LevelParser parser) {
+		this.parser = parser;
+	}
+
 	@Override
 	public void parse(Parser context, Element xmlElement) {
-		String className = xmlElement.getAttribute(CLASS);
-		String name = xmlElement.getAttribute(NAME);
-		String spriteGroup = xmlElement.getAttribute(GROUP);
+		String type = xmlElement.getAttribute(TYPE_ATTR);
 		
-		// Get the list of children and use them for constructor arguments.
+		//FIXME: Refactor: consider moving up in the hierarchy.
 		NodeList children = xmlElement.getChildNodes();
 		List<String> args = new ArrayList<String>(children.getLength());
 		for(int i = 0; i < children.getLength(); i++) {
 			args.add(getValue((Element) children.item(i)));
 		}
 		
-		parser.addSprite(name, new SpriteConstructor(parser.getConverterRack(), className,
-				spriteGroup, args));
+		PoolDeferredConstructor poolObject = new PoolDeferredConstructor(
+				parser.getSpriteConstructor(type), args);
+		
+		parser.getLevel().addToPool(poolObject);
+		
 	}
 }
