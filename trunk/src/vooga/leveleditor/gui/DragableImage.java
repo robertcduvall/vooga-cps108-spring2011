0b3450f93a391284c5b0ccc117490d89691bc41b
chatcher9356@gmail.com <chatcher9356@gmail.com@5a3452c7-76ba-512d-50cd-69e64edf86dc>
2011-04-18 22:20:27 +0000

    no longer throws the errors for dragging, still buggy

--- trunk/src/vooga/leveleditor/gui/DragableImage.java
+++ trunk/src/vooga/leveleditor/gui/DragableImage.java
@@ -23,23 +23,19 @@ public class DragableImage extends JLabel implements MouseMotionListener{
 	}
 
 	@Override
-	public void mouseDragged(MouseEvent arg0) {
-		System.out.println("im dragging");
-		
+	public void mouseDragged(MouseEvent arg0) {		
 		x = arg0.getX();
 		y = arg0.getY();
-	}
-
-	@Override
-	public void mouseMoved(MouseEvent arg0) {
-		x = arg0.getX();
-		y = arg0.getY();
-		System.out.println("im moving" + x + "  " + y);
-		//parent.remove(this);
+		parent.remove(this);
 		this.setVisible(true);
 		this.setOpaque(true);
 		this.setBounds(x, y, icon.getIconWidth(), icon.getIconWidth());
 		parent.add(this);
+	}
+
+	@Override
+	public void mouseMoved(MouseEvent arg0) {
+		
 
 	}
 }
