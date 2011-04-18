0a40c2e434b91ccf001ed478db760bf141b5a446
kevinwang29@gmail.com <kevinwang29@gmail.com@5a3452c7-76ba-512d-50cd-69e64edf86dc>
2011-04-18 22:24:35 +0000

    added drawable data to the game object

--- trunk/src/vooga/arcade/parser/Main.java
+++ trunk/src/vooga/arcade/parser/Main.java
@@ -1,5 +1,6 @@
 package vooga.arcade.parser;
 
+
 public class Main {
     public static void main(String[] args) {
         ArcadeGameObject ob = GameXMLParser.getGameData("./src/vooga/arcade/parser/game.xml");
