1b67e5fae91af3cc301aeed43816720c963e87d0
dmiller309@gmail.com <dmiller309@gmail.com@5a3452c7-76ba-512d-50cd-69e64edf86dc>
2011-04-18 17:32:23 +0000

    dep fix.

--- trunk/src/games/invaders/game/enemyFormations/Formation.java
+++
@@ -1,5 +0,0 @@
-package games.invaders.game.enemyFormations;
-
-abstract public class Formation {
-	abstract public void update(long timeElapsed);
-}
--- trunk/src/games/invaders/game/ships/Enemy.java
+++
@@ -1,9 +0,0 @@
-package games.invaders.game.ships;
-
-import com.golden.gamedev.GameObject;
-
-public class Enemy extends Ship {
-	public Enemy(GameObject gameObject, String shipImage) {
-		super(gameObject.getImage(shipImage));
-	}
-}
