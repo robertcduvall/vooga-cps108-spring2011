70a61fd46157c33d65774ae2eaf5e226c2181131
kevinwang29@gmail.com <kevinwang29@gmail.com@5a3452c7-76ba-512d-50cd-69e64edf86dc>
2011-04-20 17:28:00 +0000

    wrote game icon

--- trunk/src/vooga/arcade/parser/Main.java
+++ trunk/src/vooga/arcade/parser/Main.java
@@ -1,12 +1,22 @@
 package vooga.arcade.parser;
 
+import javax.swing.JFrame;
+
+import vooga.arcade.view.helper.GameIcon;
+
 
 public class Main {
     public static void main(String[] args) {
         ArcadeGameObject ob = GameXMLParser.getGameData("./src/vooga/arcade/parser/game.xml");
         System.out.println(ob.getData("rating"));
         ob.putData("rating", "yhg is wtf");
-        ob.start();
-        System.out.println("lololol");
+        //ob.start();
+        //System.out.println("lololol");
+        
+        JFrame frame = new JFrame();
+        frame.getContentPane().add(new GameIcon(ob));
+        frame.pack();
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setVisible(true);
     }
 }
