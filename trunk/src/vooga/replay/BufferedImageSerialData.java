ecca62e095f6f1e4d73c62bf1123ee76cb9c0229
CJAMES1224 <CJAMES1224@5a3452c7-76ba-512d-50cd-69e64edf86dc>
2011-04-20 23:39:34 +0000

    Saves images to serialized files. Click replay for cat-rolling!

---
+++ trunk/src/vooga/replay/BufferedImageSerialData.java
@@ -0,0 +1,26 @@
+package vooga.replay;
+
+import java.awt.image.BufferedImage;
+import java.io.Serializable;
+
+public class BufferedImageSerialData implements Serializable {
+
+	int width;
+	int height;
+	int[] pixels;
+
+	public BufferedImageSerialData(BufferedImage bi) {
+		width = bi.getWidth();
+		height = bi.getHeight();
+		pixels = new int[width * height];
+		int[] tmp = bi.getRGB(0, 0, width, height, pixels, 0, width);
+	}
+
+	public BufferedImage getImage() {
+		BufferedImage bi = new BufferedImage(width, height,
+				BufferedImage.TYPE_INT_ARGB); 
+		bi.setRGB(0, 0, width, height, pixels, 0, width);
+		return bi;
+	}
+
+}
