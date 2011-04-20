bf2c78ebee91697d13b86b0ab53535660bcbaafd
ncklug <ncklug@5a3452c7-76ba-512d-50cd-69e64edf86dc>
2011-04-20 17:25:28 +0000

    PHYSICS: resembles a working implementation, although not the final working implementation that we'll use. Relies on getting an instance of the current game, which apparently won't be how we do things in the final implementation.

--- src/vooga/physics/mediators/VoogaPhysicsMediator.java
+++ src/vooga/physics/mediators/VoogaPhysicsMediator.java
@@ -1,7 +1,12 @@
 package vooga.physics.mediators;
 
 import java.awt.Point;
+import java.util.HashMap;
+import java.util.Map;
+import com.golden.gamedev.Game;
+import vooga.core.VoogaGame;
 import vooga.physics.engine.BasePhysicsEngine;
+import vooga.physics.engine.NewtonianPhysicsEngine;
 import vooga.physics.interfaces.newtonian.INewtonianMovable;
 import vooga.sprites.improvedsprites.Sprite;
 import vooga.sprites.spritebuilder.components.basic.PhysicsC;
@@ -10,7 +15,35 @@ import vooga.util.math.Angle;
 
 public class VoogaPhysicsMediator {
 
-    private BasePhysicsEngine defaultPhysics;
+    private static VoogaPhysicsMediator myInstance;
+    private static Map<VoogaGame, BasePhysicsEngine> myEngines;
+    private BasePhysicsEngine getDefaultEngine(){
+        return new NewtonianPhysicsEngine();
+    }
+    
+    private VoogaPhysicsMediator(){
+        myEngines = new HashMap<VoogaGame, BasePhysicsEngine>();
+    }
+    
+    public static VoogaPhysicsMediator getInstance(){
+        if (myInstance == null)
+            myInstance = new VoogaPhysicsMediator();
+        return myInstance;
+    }
+    
+    public BasePhysicsEngine getEngine(VoogaGame game){
+        if (!myEngines.containsKey(game))
+            myEngines.put(game, getDefaultEngine());
+        return myEngines.get(game);
+    }
+    
+    public static void addEngine(VoogaGame currentGame, BasePhysicsEngine desiredEngine) {
+        myEngines.put(currentGame, desiredEngine);
+    }
+    
+    public static void removeEngine(VoogaGame currentGame, BasePhysicsEngine desiredEngine) {
+        myEngines.remove(currentGame);
+    }
 
     /**
      * General collision method. Tells the two physical objects that a collision
@@ -22,12 +55,12 @@ public class VoogaPhysicsMediator {
      * @param angleOfImpact
      * @param coefficientOfRestitution
      */
-    public void collision(Sprite object1, Sprite object2, Angle angleOfImpact, Point pointOfImpact, double coefficientOfRestitution) {
+    public void collision(VoogaGame game, Sprite object1, Sprite object2, Angle angleOfImpact, Point pointOfImpact, double coefficientOfRestitution) {
         for (IComponent c : object1.getComponents()) {
-            defaultPhysics.collision(c, getBestCollisionComponent(object2), angleOfImpact, pointOfImpact, coefficientOfRestitution);
+            myEngines.get(game).collision(c, getBestCollisionComponent(object2), angleOfImpact, pointOfImpact, coefficientOfRestitution);
         }
         for (IComponent c : object2.getComponents()) {
-            defaultPhysics.collision(c, getBestCollisionComponent(object1), angleOfImpact, pointOfImpact, coefficientOfRestitution);
+            myEngines.get(game).collision(c, getBestCollisionComponent(object1), angleOfImpact, pointOfImpact, coefficientOfRestitution);
         }
     }
 //
