1e0ec0f9c34174561e26415cad179690805eb4d6
Nosrettap99@gmail.com <Nosrettap99@gmail.com@5a3452c7-76ba-512d-50cd-69e64edf86dc>
2011-04-22 02:06:29 +0000

    Changes to level example 2 level type A and B

--- trunk/src/vooga/levels/example2/LevelTypeA.java
+++ trunk/src/vooga/levels/example2/LevelTypeA.java
@@ -2,15 +2,17 @@ package vooga.levels.example2;
 
 import java.util.Collection;
 import com.golden.gamedev.Game;
+import vooga.core.VoogaGame;
 import vooga.levels.AbstractLevel;
 import vooga.sprites.improvedsprites.Sprite;
+import vooga.sprites.spritegroups.SpriteGroup;
 
 /**
  * A basic level type which initializes all blueEnemy sprites
  */
 public class LevelTypeA extends AbstractLevel {
 
-    public LevelTypeA (Collection<Sprite> players, Game game)
+    public LevelTypeA (SpriteGroup players, VoogaGame game)
     {
         super(players, game);
     }
