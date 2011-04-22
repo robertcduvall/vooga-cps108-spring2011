a9992f67b309c48818187be6e1dfe5fad218b5b3
chatcher9356@gmail.com <chatcher9356@gmail.com@5a3452c7-76ba-512d-50cd-69e64edf86dc>
2011-04-22 13:37:25 +0000

    LevelEditor: Added jtable to popup menu to allow the editing of sprite properties

--- trunk/src/vooga/leveleditor/gui/PopupFrame.java
+++ trunk/src/vooga/leveleditor/gui/PopupFrame.java
@@ -1,12 +1,16 @@
 package vooga.leveleditor.gui;
 
 import javax.swing.JInternalFrame;
+import javax.swing.JList;
+import javax.swing.JScrollPane;
 
 public class PopupFrame extends JInternalFrame{
 	public PopupFrame(int x, int y){
 		super("Example", true, true, true, true);
 		this.setSize(200, 200);
 		this.setLocation(x, y);
+		JScrollPane scroll = new JScrollPane(new SpriteProperties());
+		this.add(scroll);
 		this.show();
 	}
 }
