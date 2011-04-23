e04c8afc4fcc05a7f4ead8752f29325c81751088
jsf.fan <JSF.fan@gmail.com>
2011-04-23 19:17:39 +0000

    skeleton xml file added for pong

--- src/games/pong/sprites/AIPaddle.java
+++ src/games/pong/sprites/AIPaddle.java
@@ -1,7 +1,14 @@
 package games.pong.sprites;
 
 import vooga.sprites.improvedsprites.Sprite;
+import vooga.sprites.spritebuilder.components.PongTargetC;
 
 public class AIPaddle extends Sprite{
-
+	public AIPaddle(Sprite ball) {
+		
+		//set behavior
+		PongTargetC trackBall = new PongTargetC();
+		trackBall.setTarget(ball);
+		addComponent(trackBall);
+	}
 }
