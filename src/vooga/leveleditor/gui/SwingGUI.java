3328d9434580d36356ee532761a93f51b4b58378
hpkuarg <hpkuarg@gmail.com>
2011-04-24 23:11:57 +0000

    LevelEditor: Some minor refactoring.

--- src/vooga/leveleditor/gui/SwingGUI.java
+++ src/vooga/leveleditor/gui/SwingGUI.java
@@ -17,12 +17,6 @@ import javax.swing.border.EtchedBorder;
 public class SwingGUI extends JFrame
 {
 
-    private JMenuBar menubar;
-
-    private JToolBar toolbar;
-
-    private JLabel statusbar;
-
     public SwingGUI()
     {
         /*
@@ -37,49 +31,11 @@ public class SwingGUI extends JFrame
         this.setJMenuBar(new MenuBar());
 
         /*
-         * Set up the toolbar.
-         */
-        JToolBar toolbar = new ToolBar();
-        this.add(toolbar, BorderLayout.NORTH);
-
-        /*
          * Set up the drawing board, which is the main area of action.
          */
         DrawingBoard db = new DrawingBoard(this);
         add(db, BorderLayout.CENTER);
-
-        /*
-         * Set up the status bar.
-         */
-        this.statusbar = new StatusBar();
-        this.add(statusbar, BorderLayout.SOUTH);
     }
 
-    /**
-     * The status bar on the bottom of the window. Displays helpful messages
-     * to the user, including the mouse coordinates.
-     */
-    private class StatusBar extends JLabel
-    {
-        public StatusBar()
-        {
-            super();
-            setText("Ready");
-        }
-    }
-
-    public void setStatusBar(String message)
-    {
-        statusbar.setText(message);
-    }
-
-    public static void main(String[] args)
-    {
-        SwingGUI g = new SwingGUI();
-
-        g.pack();
-        g.setVisible(true);
-        g.setExtendedState(g.getExtendedState() | JFrame.MAXIMIZED_BOTH);
-    }
 
 }
--- src/vooga/leveleditor/gui/Viewport.java
+++ src/vooga/leveleditor/gui/Viewport.java
@@ -2,6 +2,7 @@ package vooga.leveleditor.gui;
 
 import java.awt.*;
 import java.awt.event.*;
+import java.awt.image.BufferedImage;
 import java.util.ArrayList;
 
 import javax.swing.*;
@@ -15,24 +16,54 @@ import javax.swing.event.MouseInputListener;
  */
 public class Viewport extends JLayeredPane
 {
+
+    public static final int LAYER_GREENSCREEN = -100;
+    public static final int LAYER_BACKGROUND = 0;
+    public static final int LAYER_SPRITES = 100;
+    public static final int LAYER_DRAG = 500;
+
     private int x,y;
     private DrawingBoard owner;
     private ArrayList<DraggableImage> mySprites;
-    
-    public Viewport(DrawingBoard owner, int width, int height)
+
+    public Viewport(DrawingBoard owner, BufferedImage bgimage)
     {
         super();
+
+        int width = bgimage.getWidth();
+        int height = bgimage.getHeight();
+
+        /*
+         * Construct the greenscreen. This shows the user where the end of the
+         * level is, and it extends 100 pixels each side past the boundaries
+         * of the background image.
+         */
+        JLabel greenscreen = new JLabel();
+        greenscreen.setBackground(Color.GREEN);
+        greenscreen.setOpaque(true);
+        greenscreen.setBounds(0, 0, width+200, height+200);
+
+        /*
+         * Add the greenscreen and the background.
+         */
+        this.add(greenscreen, LAYER_GREENSCREEN);
+        JLabel background = new JLabel(new ImageIcon(bgimage));
+        background.setOpaque(true);
+        background.setBounds(100, 100, width, height);
+        this.add(background, LAYER_BACKGROUND);
+
+
         mySprites = new ArrayList<DraggableImage>();
         this.owner = owner;
         setPreferredSize(new Dimension(width, height));
         addMouseMotionListener(new MouseTracker());
         addMouseListener(new MouseClick());
     }
-    
+
     void addImage(DraggableImage i){
-    	mySprites.add(i);
+        mySprites.add(i);
     }
-    
+
     /**
      * Follows the mouse and updates the status bar with the mouse coordinates.
      */
@@ -44,7 +75,7 @@ public class Viewport extends JLayeredPane
             y = e.getY();
             owner.setStatusBar(String.format("(%d, %d)", x, y));
         }
-        
+
         public void mouseDragged(MouseEvent e)
         {
             return;
@@ -52,48 +83,48 @@ public class Viewport extends JLayeredPane
     }
     private class MouseClick implements MouseListener{
 
-    	/**
-    	 * If the mouse is hovering over the image on the viewport and is clicked,
-    	 * set up that image to become draggable.
-    	 */
-		@Override
-		public void mouseClicked(MouseEvent arg0) {
-			/*if(!arg0.isControlDown()){
+        /**
+         * If the mouse is hovering over the image on the viewport and is clicked,
+         * set up that image to become draggable.
+         */
+        @Override
+        public void mouseClicked(MouseEvent arg0) {
+            /*if(!arg0.isControlDown()){
 				moveSelectedSprite();
 			}
 			if(arg0.isControlDown()){
 				editSelectedSprite();
 			}*/
-			for(int i=0; i<mySprites.size();i++){
-				if(!arg0.isControlDown()){
-					if(mySprites.get(i).moveIfSelected(x, y));
-				}
-				if(arg0.isControlDown()){
-					mySprites.get(i).setSpriteProperties(x, y);
-				}
-			}
-		}
+            for(int i=0; i<mySprites.size();i++){
+                if(!arg0.isControlDown()){
+                    if(mySprites.get(i).moveIfSelected(x, y));
+                }
+                if(arg0.isControlDown()){
+                    mySprites.get(i).setSpriteProperties(x, y);
+                }
+            }
+        }
 
-		/**
-		 * Brings up a new internal frame that serves as a popup to allow the user to
-		 * edit the sprites properties in a text file.
-		 * 
-		 * Currently unused
-		 */
-		/*
+        /**
+         * Brings up a new internal frame that serves as a popup to allow the user to
+         * edit the sprites properties in a text file.
+         * 
+         * Currently unused
+         */
+        /*
 		private void editSelectedSprite() {
 			for(int i=0; i<mySprites.size();i++){
 				mySprites.get(i).setSpriteProperties(x, y);
 			}
 		}
-		*/
-		/*
-		 * FIXME: iterate through the for loop backwards not sure if this is needed
-		 * anymore
-		 * 
-		 * currently unused
-		 */
-		/*
+         */
+        /*
+         * FIXME: iterate through the for loop backwards not sure if this is needed
+         * anymore
+         * 
+         * currently unused
+         */
+        /*
 		private void moveSelectedSprite() {
 			if(mySprites.size()>0){
 				for(DraggableImage i: mySprites){
@@ -101,31 +132,31 @@ public class Viewport extends JLayeredPane
 				}
 			}
 		}
-		*/
-		/**
-		 * the rest of these methods are unneeded, but come standard with the
-		 * MouseListener interface
-		 */
-		@Override
-		public void mouseEntered(MouseEvent arg0) {
-			return;
-		}
+         */
+        /**
+         * the rest of these methods are unneeded, but come standard with the
+         * MouseListener interface
+         */
+        @Override
+        public void mouseEntered(MouseEvent arg0) {
+            return;
+        }
 
-		@Override
-		public void mouseExited(MouseEvent arg0) {
-			return;
-		}
+        @Override
+        public void mouseExited(MouseEvent arg0) {
+            return;
+        }
 
-		@Override
-		public void mousePressed(MouseEvent arg0) {
-			
-			return;
-		}
+        @Override
+        public void mousePressed(MouseEvent arg0) {
 
-		@Override
-		public void mouseReleased(MouseEvent arg0) {
-			return;
-		}
+            return;
+        }
+
+        @Override
+        public void mouseReleased(MouseEvent arg0) {
+            return;
+        }
 
     }
 
