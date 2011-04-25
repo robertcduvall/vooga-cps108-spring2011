de27b21e9f395a3071fc93e4c5f146fb9bf8a3bf
yhgoh88@gmail.com <yhgoh88@gmail.com@5a3452c7-76ba-512d-50cd-69e64edf86dc>
2011-04-25 20:14:47 +0000

    reverting incorrect implementation of tetris and ugly braces.  Ten Commandments of Computer Ethics #2: Thou shalt not interfere with other people's computer work.

--- trunk/src/games/tetris/src/KeyActions/HardDropPiece.java
+++ trunk/src/games/tetris/src/KeyActions/HardDropPiece.java
@@ -2,15 +2,18 @@ package games.tetris.src.KeyActions;
 
 import games.tetris.src.Context.ITetrisContext;
 
-public class HardDropPiece implements IKeyAction {
-	@Override
-	public void performKeyPressed(ITetrisContext tc, long elapsedTime) {
-		tc.getActiveMino().hardDrop(tc.getTetrisMatrix());
-		tc.getTetrisMatrix().lockMino(tc.getActiveMino(), tc);
-		tc.generateNextBlock();
-	}
+public class HardDropPiece implements IKeyAction
+{
+        @Override
+        public void performKeyPressed(ITetrisContext tc, long elapsedTime)
+        {
+                tc.getActiveMino().hardDrop(tc.getTetrisMatrix());
+                tc.getTetrisMatrix().lockMino(tc.getActiveMino(),tc);
+                tc.generateNextBlock();
+        }
 
-	@Override
-	public void performKeyDown(ITetrisContext tc, long elapsedTime) {
-	}
+        @Override
+        public void performKeyDown(ITetrisContext tc, long elapsedTime)
+        {
+        }
 }
