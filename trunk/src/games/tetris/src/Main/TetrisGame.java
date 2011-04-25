726d4e58ef009855ba93cfc8293e18179ebed5c1
dmiller309@gmail.com <dmiller309@gmail.com@5a3452c7-76ba-512d-50cd-69e64edf86dc>
2011-04-25 18:03:23 +0000

    Made next block launch when current block hits the bottom. Also, fixed curly brace formatting.  Yeeeeah! d:Dk

--- trunk/src/games/tetris/src/Main/TetrisGame.java
+++ trunk/src/games/tetris/src/Main/TetrisGame.java
@@ -19,9 +19,6 @@ import java.lang.reflect.Constructor;
 import java.util.HashMap;
 import java.util.Map;
 
-
-
-
 import com.golden.gamedev.Game;
 import com.golden.gamedev.GameLoader;
 import com.golden.gamedev.object.Background;
@@ -32,16 +29,15 @@ import com.golden.gamedev.object.SpriteGroup;
 import com.golden.gamedev.object.Timer;
 import com.golden.gamedev.object.background.ColorBackground;
 
-public class TetrisGame extends Game implements ITetrisContext
-{
-	private ResourceManager resources = new ResourceManager("games.tetris.TetrisResource");
-	private ResourceManager keypressResource = new ResourceManager(
+public class TetrisGame extends Game implements ITetrisContext {
+	private final ResourceManager resources = new ResourceManager(
+			"games.tetris.TetrisResource");
+	private final ResourceManager keypressResource = new ResourceManager(
 			"games.tetris.KeypressResource");
 
-	private Map<Integer, IKeyAction> keyMapping;
+	private Map<Integer, IKeyAction> keyMappings;
 
 	private PlayField playfield;
-	private Background windowBackground;
 
 	private SpriteGroup matrixGroup;
 	private SpriteGroup holdGroup;
@@ -50,8 +46,8 @@ public class TetrisGame extends Game implements ITetrisContext
 	private Sprite matrixBorder;
 
 	private Timer blockGravityTimer;
-//	private Timer blockLockTimer;
-//	private SystemTimer gameTimer;
+	// private Timer blockLockTimer;
+	// private SystemTimer gameTimer;
 	private Timer dasTimer;
 	private Timer arrTimer;
 	private Timer softDropTimer;
@@ -75,8 +71,8 @@ public class TetrisGame extends Game implements ITetrisContext
 	/**************************** GAME SKELETON *********************************/
 	/****************************************************************************/
 
-	public void initResources()
-	{
+	@Override
+	public void initResources() {
 		this.initGameEssentials();
 		this.initGameTimers();
 
@@ -90,56 +86,43 @@ public class TetrisGame extends Game implements ITetrisContext
 		gameOver = false;
 	}
 
-	public void reset()
-	{
-		
+	public void reset() {
+
 		initResources();
 	}
 
-	private void initKeyMappings()
-	{
-		keyMapping = new HashMap<Integer, IKeyAction>();
-		for (String s : keypressResource.keySet())
-		{
+	private void initKeyMappings() {
+		keyMappings = new HashMap<Integer, IKeyAction>();
+		for (String s : keypressResource.keySet()) {
 			int key = 0;
-			try
-			{
+			try {
 				key = Integer.parseInt(s);
-			}
-			catch (NumberFormatException e)
-			{
+			} catch (NumberFormatException e) {
 				continue;
 			}
 
 			String className = keypressResource.getString(s);
 			String packagePath = keypressResource.getString("PackageName");
 			Class<?> c = null;
-			try
-			{
+			try {
 				c = Class.forName(packagePath + "." + className);
 				Constructor<?> ctr = c.getConstructor();
 				IKeyAction am = (IKeyAction) ctr.newInstance();
-				keyMapping.put(key, am);
-			}
-			catch (Exception e)
-			{
+				keyMappings.put(key, am);
+			} catch (Exception e) {
 			}
 		}
 	}
 
 	@Override
-	public void generateNextBlock()
-	{
+	public void generateNextBlock() {
 		activeMino.setMino(lookAheadMinos.getNextBlock());
-		if (!tetrisMatrix.isLegalPosition(activeMino.getCurrentMino()))
-		{
+		if (!tetrisMatrix.isLegalPosition(activeMino.getCurrentMino())) {
 			gameOver = true;
 		}
 	}
 
-	private void initSpriteComponents()
-	{
-
+	private void initSpriteComponents() {
 		holdGroup = new SpriteGroup("HoldGroup");
 		matrixGroup = new SpriteGroup("BlockMatrixGroup");
 		lookAheadGroup = new SpriteGroup("lookAheadGroup");
@@ -156,8 +139,7 @@ public class TetrisGame extends Game implements ITetrisContext
 
 	}
 
-	private void initGameTimers()
-	{
+	private void initGameTimers() {
 		arrDelay = resources.getInteger("DefaultArrDelay");
 		dasDelay = resources.getInteger("DefaultDasDelay");
 		softDropDelay = resources.getInteger("DefaultSoftDropDelay");
@@ -168,128 +150,121 @@ public class TetrisGame extends Game implements ITetrisContext
 		blockGravityTimer = new Timer(blockGravityDelay);
 	}
 
-	private void initGameEssentials()
-	{
+	private void initGameEssentials() {
 		playfield = new PlayField();
-		windowBackground = new ColorBackground(Color.black,
-				resources.getInteger("BackgroundWidth"),
-				resources.getInteger("BackgroundHeight"));
+		Background windowBackground = new ColorBackground(Color.black,
+				resources.getInteger("BackgroundWidth"), resources
+						.getInteger("BackgroundHeight"));
 
 		BufferedImage matrixImage = getImage(resources
 				.getString("BackgroundImage"));
-		matrixBackground = new Sprite(matrixImage,
-				resources.getInteger("MatrixSpawnCenterWidth") - 10, 100);
+		matrixBackground = new Sprite(matrixImage, resources
+				.getInteger("MatrixSpawnCenterWidth") - 10, 100);
 
 		BufferedImage matrixBorderImage = ImageFactory.drawMatrixBorder(260,
 				440);
-		matrixBorder = new Sprite(matrixBorderImage,
-				resources.getInteger("MatrixSpawnCenterWidth") - 41, 72);
+		matrixBorder = new Sprite(matrixBorderImage, resources
+				.getInteger("MatrixSpawnCenterWidth") - 41, 72);
 		matrixBorder.setLayer(2);
 
 		playfield.setBackground(windowBackground);
 
-		font = fontManager.getFont(
-				getImages(resources.getString("FontImage"), 20, 3),
-				resources.getString("FontMapping"));
+		font = fontManager.getFont(getImages(resources.getString("FontImage"),
+				20, 3), resources.getString("FontMapping"));
 	}
 
-	public void update(long elapsedTime)
-	{
+	@Override
+	public void update(long elapsedTime) {
 		this.checkKeyPresses(elapsedTime);
 
-		if (!gameOver)
-		{
+		if (!gameOver) {
 			playfield.update(elapsedTime);
 
 			// Make Blocks fall.
-			if (blockGravityTimer.action(elapsedTime))
-				activeMino.moveY(-1, tetrisMatrix);
-		}
-	}
-
-	private void checkKeyPresses(long elapsedTime)
-	{
-		for (Integer i : keyMapping.keySet())
-		{
-			if (keyPressed(i))
-			{
-				keyMapping.get(i).performKeyPressed(this, elapsedTime);
-			}
-			if (keyDown(i))
-			{
-				keyMapping.get(i).performKeyDown(this, elapsedTime);
+			if (blockGravityTimer.action(elapsedTime)) {
+				boolean result = activeMino.moveY(-1, tetrisMatrix);
+				if (!result) {
+					// TODO: This is redundant from
+					// HardDropPiece.performKeyPressed
+					getActiveMino().hardDrop(getTetrisMatrix());
+					getTetrisMatrix().lockMino(getActiveMino(), this);
+					generateNextBlock();
+				}
 			}
 		}
 	}
 
-	public void render(Graphics2D g)
-	{
+	private void checkKeyPresses(long elapsedTime) {
+		for (Integer i : keyMappings.keySet()) {
+			if (keyPressed(i)) {
+				keyMappings.get(i).performKeyPressed(this, elapsedTime);
+			}
+			if (keyDown(i)) {
+				keyMappings.get(i).performKeyDown(this, elapsedTime);
+			}
+		}
+	}
+
+	@Override
+	public void render(Graphics2D g) {
 		playfield.render(g);
 		// draw info text
 		font.drawString(g, "DELAYED AUTOSHIFT: " + Integer.toString(dasDelay),
 				10, 10);
 		font.drawString(g, "AUTO REPEAT RATE: " + Integer.toString(arrDelay),
 				10, 30);
-		font.drawString(g, "NEXT",
-				resources.getInteger("MatrixSpawnCenterWidth") + 240, 90);
+		font.drawString(g, "NEXT", resources
+				.getInteger("MatrixSpawnCenterWidth") + 240, 90);
 		font.drawString(g, "HOLD", 20, 90);
-		if (gameOver)
-		{
-			font.drawString(g,
-					"NOOBFACE. GAME OVER. PRESS R TO RESTART: ", 100, 250);
+		if (gameOver) {
+			font.drawString(g, "NOOBFACE. GAME OVER. PRESS R TO RESTART: ",
+					100, 250);
 		}
 
 	}
-	
-	@Override	
-	protected void notifyExit()
-	{
-		
+
+	@Override
+	protected void notifyExit() {
+
 	}
+
 	/****************************************************************************/
 	/***************************** START-POINT **********************************/
 	/****************************************************************************/
 
-	public static void main(String[] args)
-	{
+	public static void main(String[] args) {
 		GameLoader game = new GameLoader();
 		game.setup(new TetrisGame(), new Dimension(800, 600), false);
 		game.start();
 	}
 
 	@Override
-	public ActiveMino getActiveMino()
-	{
+	public ActiveMino getActiveMino() {
 		return activeMino;
 	}
 
 	@Override
-	public TetrisMatrix getTetrisMatrix()
-	{
+	public TetrisMatrix getTetrisMatrix() {
 		return tetrisMatrix;
 	}
 
 	@Override
-	public Timer getDasTimer()
-	{
+	public Timer getDasTimer() {
 		return dasTimer;
 	}
 
 	@Override
-	public Timer getArrTimer()
-	{
+	public Timer getArrTimer() {
 		return arrTimer;
 	}
 
 	@Override
-	public Timer getSoftDropTimer()
-	{
+	public Timer getSoftDropTimer() {
 		return softDropTimer;
 	}
 
 	@Override
-	public void adjustDasBy(int i)
-	{
+	public void adjustDasBy(int i) {
 		dasDelay += i;
 		if (dasDelay < 0)
 			dasDelay = 0;
@@ -297,14 +272,12 @@ public class TetrisGame extends Game implements ITetrisContext
 	}
 
 	@Override
-	public boolean holdMino()
-	{
+	public boolean holdMino() {
 		return heldMinoManager.swapHeldMino(activeMino, this);
 	}
 
 	@Override
-	public void setHoldUsed(boolean b)
-	{
+	public void setHoldUsed(boolean b) {
 		heldMinoManager.setHoldUsed(b);
 	}
 }
