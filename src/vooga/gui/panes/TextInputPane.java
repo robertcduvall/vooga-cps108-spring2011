ccaefe3c820ba2075ad826d3d170540331ed60db
Altfiv3 <AltFiv3@gmail.com>
2011-04-25 17:46:54 +0000

    trying again with TextInputPane

--- src/vooga/gui/panes/TextInputPane.java
+++ src/vooga/gui/panes/TextInputPane.java
@@ -1,18 +1,51 @@
 package vooga.gui.panes;
 
+import java.awt.Dimension;
+import java.awt.Graphics2D;
+import java.awt.image.BufferedImage;
+
+import com.golden.gamedev.gui.TTextField;
+import com.golden.gamedev.gui.toolkit.FrameWork;
+
 import vooga.gui.PaneManager;
+import vooga.gui.util.VoogaButton;
 
 /**
- * Type of popover that allows a user to input a value, and when 'accpt' button
- * is pressed (customizable look) returns the input to its parent.
+ * Type of popover that allows a user to input a value, and when 'accpt' 
+ * button is pressed (customizable look) returns the input to its parent.
  * @author Dave Crowe, David Colon-Smith
  *
  */
-public class TextInputPane extends PopoverPane {
-
-	public TextInputPane(PaneManager parent) {
-		super(parent);
-		// TODO -Stuff!
-	}
-
+public class TextInputPane extends PopoverPane
+{
+    FrameWork textInputPaneFrame;
+    TTextField tTextBox;
+    VoogaButton acceptButton;
+   
+    public TextInputPane(PaneManager parent, BufferedImage acceptImage)
+    {
+        super(parent);
+        acceptButton = new VoogaButton(acceptImage, "Accept", new Dimension(50, 100));
+    }
+   
+    public void initResources()
+    {
+        /*
+         * The following error is COMPLETELY INTENTIONAL. Granted, it may sound really really
+         * stupid, especially at (just checked the time) 4:05 AM, but it shouldn't kill
+         * anything from anywhere. Oh, and I need to figure out what replaces GameObject for
+         * this case.
+         */
+        textInputPaneFrame = new FrameWork(myParent.getParent().bsInput, myParent.getWidth(), myParent.getHeight());
+        tTextBox = new TTextField("", myParent.getWidth()/2-50, myParent.getHeight()/2+20, 110, 25);
+        textInputPaneFrame.add(tTextBox);
+    }
+   
+    @Override
+    public void render(Graphics2D g)
+    {
+        super.render(g);
+        tTextBox.render(g);
+        acceptButton.render(g);
+    }
 }
