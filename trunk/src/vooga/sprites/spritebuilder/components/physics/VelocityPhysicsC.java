85b0252ac1fde69594b70f248b06a7e032ccb747
moongwiff@gmail.com <moongwiff@gmail.com@5a3452c7-76ba-512d-50cd-69e64edf86dc>
2011-04-25 02:53:46 +0000

    PHYSICS: Added VelocityPhysicsC and FrictionCollisionBehavior

---
+++ trunk/src/vooga/sprites/spritebuilder/components/physics/VelocityPhysicsC.java
@@ -0,0 +1,50 @@
+package vooga.sprites.spritebuilder.components.physics;
+
+import vooga.physics.collisionBehavior.MovableCollisionBehavior;
+import vooga.physics.fieldBehavior.EmptyFieldBehavior;
+import vooga.physics.forceBehavior.EmptyForceBehavior;
+import vooga.physics.util.Velocity;
+import vooga.sprites.improvedsprites.Sprite;
+import vooga.util.buildable.components.BasicComponent;
+
+/**
+ * A physics component that just has velocity. No mass.
+ * 
+ * @author Anne Weng
+ * 
+ */
+public class VelocityPhysicsC extends GeneralPhysicsC{
+
+    /**
+     * Creates a VelocityPhysicsC with given velocity and mass and the default 'on' state.
+     * @param velocity
+     * @param mass
+     */
+    public VelocityPhysicsC(Velocity velocity, double mass) {
+        this(velocity, true);
+    }
+    
+    /**
+     * Creates a VelocityPhysicsC with given velocity, mass, and state.
+     * @param velocity
+     * @param mass
+     * @param isOn
+     */
+    public VelocityPhysicsC(Velocity velocity, boolean isOn) {
+        super(new EmptyForceBehavior(), new EmptyFieldBehavior(), new MovableCollisionBehavior(velocity), isOn);
+    }
+
+    @Override
+    protected int compareTo(BasicComponent o) {
+        // TODO: do we use this to compare whether a component is more specific.
+        // for physics than another
+        return 0;
+    }
+
+    @Override
+    public void update(Sprite s, long elapsedTime) {
+        super.update(s, elapsedTime);
+        //Updates the current velocity for the collision behavior
+        super.myCollisionBehavior.updateBehavior(getSpriteVelocityForPhysics(s));
+    }
+}
