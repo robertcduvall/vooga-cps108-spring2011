8429e8048eedf19089ebf292718d79da0d34c2d2
ncklug <ncklug@5a3452c7-76ba-512d-50cd-69e64edf86dc>
2011-04-25 02:33:27 +0000

    PHYSICS: Cleanup as a result of changes

--- src/vooga/physics/collisionBehavior/EmptyCollisionBehavior.java
+++ src/vooga/physics/collisionBehavior/EmptyCollisionBehavior.java
@@ -5,9 +5,25 @@ import vooga.physics.AbstractBehavior;
 import vooga.physics.util.Velocity;
 import vooga.util.math.Angle;
 
+/**
+ * Exactly what the class name says. Defines collision behavior which does nothing. To be used in Vooga
+ * in the case where a physics component does not define collision behavior.
+ * 
+ * @author Nathan Klug
+ *
+ */
 public class EmptyCollisionBehavior extends AbstractBehavior{
 
-    public Velocity collisionToVelocityChange(EmptyCollisionBehavior collisionBehavior, Angle angleOfImpact, Point pointOfImpact, double coefficientOfRestitution) {
+    /**
+     * Returns the change in the current object's velocity, given a collision with another object's collision behavior.
+     * 
+     * @param otherCollisionBehavior the collision behavior of the other object
+     * @param angleOfImpact
+     * @param pointOfImpact
+     * @param coefficientOfRestitution
+     * @return the change in velocity that occurs as a result
+     */
+    public Velocity collisionToVelocityChange(EmptyCollisionBehavior otherCollisionBehavior, Angle angleOfImpact, Point pointOfImpact, double coefficientOfRestitution) {
         return new Velocity(0, new Angle());
     }
 }
