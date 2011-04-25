f89970b2604d51f28914464e158856767fd1dc8d
yhgoh88@gmail.com <yhgoh88@gmail.com@5a3452c7-76ba-512d-50cd-69e64edf86dc>
2011-04-25 03:06:47 +0000

    About Panel HTML PAGE RAWRRRRRRRRRRR

---
+++ trunk/src/vooga/arcade/html/AboutHTMLPage.java
@@ -0,0 +1,49 @@
+package vooga.arcade.html;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+
+import vooga.arcade.parser.ArcadeGameObject;
+
+public class AboutHTMLPage
+{
+	private static HTMLGenerator htmlGenerator = new HTMLGenerator();
+
+	private static String[] aboutFields = { "Title", "Genre", "Author",
+			"Date Created", "Version", "Language", "Price" };
+
+	public static String generateHTMLFile(ArcadeGameObject ago)
+	{
+		for (String s : aboutFields)
+		{
+			String t = removeAllSpaces(s);
+			htmlGenerator.addTag("b", s + ": ");
+			htmlGenerator.addText(ago.getData(t));
+			htmlGenerator.addLineBreak();
+		}
+
+		// TODO: FIX THISSSS OAWKEMRLFSDMLKAERJWLKFJSLKDF
+		File file = new File("src/vooga/arcade/html/about.html");
+		try
+		{
+			FileWriter ofstream = new FileWriter(file);
+			BufferedWriter out = new BufferedWriter(ofstream);
+			out.write(htmlGenerator.toString());
+			out.close();
+		}
+		catch (Exception e)
+		{
+		}
+		return file.getAbsolutePath();
+	}
+
+	private static String removeAllSpaces(String s)
+	{
+		String[] ar = s.split("\\s+");
+		String tr = "";
+		for (String t : ar)
+			tr += t;
+		return tr;
+	}
+}
