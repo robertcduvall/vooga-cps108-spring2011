69e499db7bcd50b37082b4c109b0c7049ccb8623
jgenk90 <jgenk90@5a3452c7-76ba-512d-50cd-69e64edf86dc>
2011-04-27 10:07:40 +0000

    forgot one change

--- src/vooga/sprites/improvedsprites/Sprite.java
+++ src/vooga/sprites/improvedsprites/Sprite.java
@@ -522,7 +522,7 @@ public class Sprite extends BaseSprite
     	AffineTransform aTransform = new AffineTransform();
         aTransform.translate((int) this.getX() +width/2, 
                              (int) this.getY()+height/2);
-        aTransform.rotate(Math.toRadians(this.getAngle()));
+        aTransform.rotate(Math.toRadians(this.getAngle()+90));
         aTransform.translate((int) -width/2, 
                              (int) -height/2);
         g.drawImage(image.getScaledInstance(width, height, 0),aTransform,null);
