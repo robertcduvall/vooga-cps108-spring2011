5633874d5ca53549945e58a07060f57cfc48ae27
yhgoh88@gmail.com <yhgoh88@gmail.com@5a3452c7-76ba-512d-50cd-69e64edf86dc>
2011-04-29 23:12:43 +0000

    Fixing error.... how did this get here?

--- trunk/src/vooga/arcade/parser/Main.java
+++ trunk/src/vooga/arcade/parser/Main.java
@@ -2,21 +2,13 @@ package vooga.arcade.parser;
 
 import javax.swing.JFrame;
 
+import vooga.arcade.controller.ArcadeController;
 import vooga.arcade.view.gamePanel.ArcadeGamePanel;
 
 
 public class Main {
     public static void main(String[] args) {
-        ArcadeGameObject ob = GameXMLParser.getGameData("./src/vooga/arcade/parser/game.xml");
-        System.out.println(ob.getData("rating"));
-        ob.writeData("rating", "yhg is wtf");
-        //ob.start();
-        //System.out.println("lololol");
-        
-        JFrame frame = new JFrame();
-        frame.getContentPane().add(new ArcadeGamePanel(ob));
-        frame.pack();
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setVisible(true);
+        ArcadeController ac = new ArcadeController("Hi","test",800,600);
+
     }
 }
