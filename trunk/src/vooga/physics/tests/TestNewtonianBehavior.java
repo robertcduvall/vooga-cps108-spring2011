9d8cf817885c8428ece68b68824a5698d25c9815
moongwiff@gmail.com <moongwiff@gmail.com@5a3452c7-76ba-512d-50cd-69e64edf86dc>
2011-04-29 19:09:31 +0000

    PHYSICS: Getting our tests back online. Also fixed an initialization error EmptyPhysicsC.

---
+++ trunk/src/vooga/physics/tests/TestNewtonianBehavior.java
@@ -0,0 +1,58 @@
+package vooga.physics.tests;
+
+import static org.junit.Assert.*;
+
+import java.awt.Point;
+import java.util.ArrayList;
+import java.util.Collection;
+
+import org.junit.Test;
+
+import vooga.physics.collisionBehavior.EmptyCollisionBehavior;
+import vooga.physics.util.Force;
+import vooga.physics.util.Velocity;
+import vooga.sprites.spritebuilder.components.physics.BasicPhysicsC;
+import vooga.util.math.Angle;
+
+/**
+ * Test class for Newtonian behavior.
+ * 
+ * @author Anne Weng
+ *
+ */
+public class TestNewtonianBehavior {
+
+    @Test
+    public void testNewtonianForce(){
+        BasicPhysicsC basic = new BasicPhysicsC(new Velocity(0,new Angle(0)), 1);
+        //Velocity of 0, mass of 1
+        basic.applyForce(new Force(10,new Angle(Math.PI)), 1);
+        //Applies force with magnitude 10 pointing west
+        Velocity delta = basic.getDeltaVelocity();
+        assertEquals("Delta magnitude single application", 10, delta.getMagnitude(), 0);
+        assertEquals("Delta angle single application", Math.PI, delta.getAngle().getRadians(),0);
+
+        Collection<Force> forces = new ArrayList<Force>();
+        forces.add(new Force(10,new Angle(Math.PI/2)));
+        forces.add(new Force(10,new Angle(0)));
+        forces.add(new Force(10,new Angle(Math.PI*1.5)));
+        basic.applyForces(forces, 1);
+        delta = basic.getDeltaVelocity();
+        assertEquals("Delta magnitude multiple applications", 0, delta.getMagnitude(),Math.pow(10,-14));
+    }
+
+    @Test
+    /*
+     * So applyCollision isn't getting overridden...
+     */
+    public void testPhysicalCollision(){
+        BasicPhysicsC basic1 = new BasicPhysicsC(new Velocity(10,new Angle(0)), 1);
+        BasicPhysicsC basic2 = new BasicPhysicsC(new Velocity(5,new Angle(Math.PI)), 1);
+        Angle collisionAngle = new Angle(Math.PI/2);
+        basic1.applyCollision(basic2.getCollisionBehavior(), collisionAngle, new Point(0,0), 1);
+        basic2.applyCollision(basic1.getCollisionBehavior(), collisionAngle, new Point(0,0), 1);
+        Velocity delta1 = basic1.getDeltaVelocity();
+        assertEquals("Delta1 magnitude", 5, delta1.getMagnitude(),0);
+        assertEquals("Delta1 angle", Math.PI, delta1.getAngle().getRadians(),0);
+    }
+}
