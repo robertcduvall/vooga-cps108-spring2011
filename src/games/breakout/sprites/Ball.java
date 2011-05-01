e85e20d5c2042627a6d5a5c7c55a1b19932f3a19
misha.p.l <misha.p.l@gmail.com>
2011-05-01 19:21:21 +0000

    Breakout has multiple levels and fancier graphics.

--- src/games/breakout/sprites/Ball.java
+++ src/games/breakout/sprites/Ball.java
@@ -3,12 +3,25 @@ package games.breakout.sprites;
 import java.awt.image.BufferedImage;
 import vooga.sprites.improvedsprites.Sprite;
 
+/**
+ * A breakout ball.
+ * 
+ * @author Misha
+ */
 @SuppressWarnings("serial")
 public class Ball extends Sprite
 {
-    private static final Double BALL_SPEED = 0.4D;
+    /**
+     * The ball's speed, in pixels per ms.
+     */
+    public static final Double BALL_SPEED = 0.4D;
     
-    
+    /**
+     * Creates a ball moving straight up and sitting on 
+     * top of the point (x,y)
+     * 
+     * @param image The image to use for the ball.
+     */
     public Ball(BufferedImage image, int x, int y)
     {
         super(image,x,y);
@@ -18,26 +31,44 @@ public class Ball extends Sprite
         setAbsoluteSpeed(BALL_SPEED);
     }
     
+    /**
+     * @return The radius of the ball.
+     */
     public double getRadius() {return getWidth()/2D;}
     
+    /**
+     * Bounce the ball upwards from an obstacle
+     * below the ball.
+     */
     public void bounceUp()
     {
         if (getVerticalSpeed() > 0)
             setVerticalSpeed(-getVerticalSpeed());
     }
     
+    /**
+     * Bounce the ball downwards from an obstacle above
+     * the ball (this exists as a separate method so
+     * that multiple collisions don't cancel out).
+     */
     public void bounceDown()
     {
         if (getVerticalSpeed() < 0)
             setVerticalSpeed(-getVerticalSpeed());
     }
     
+    /**
+     * Bounce the ball to the left.
+     */
     public void bounceLeft()
     {
         if (getHorizontalSpeed() > 0)
             setHorizontalSpeed(-getHorizontalSpeed());
     }
     
+    /**
+     * Bounce the ball to the right.
+     */
     public void bounceRight()
     {
         if (getHorizontalSpeed() < 0)
