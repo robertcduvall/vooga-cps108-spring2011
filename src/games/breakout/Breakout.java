1a9883a5bbfa6b1a105dd3db5a59ce6b9200bd68
misha.p.l <misha.p.l@gmail.com>
2011-05-01 03:09:36 +0000

    started working on prettier levels

--- src/games/breakout/Breakout.java
+++ src/games/breakout/Breakout.java
@@ -4,6 +4,7 @@ import games.breakout.sprites.Paddle;
 import java.awt.Dimension;
 import vooga.core.VoogaGame;
 import vooga.core.event.EventManager;
+import vooga.resources.images.ImageLoader;
 import vooga.sprites.spritebuilder.components.collisions.CollisionCircleC;
 import vooga.sprites.spritegroups.SpriteGroup;
 
@@ -11,7 +12,12 @@ public class Breakout extends VoogaGame
 {
     {distribute = true;}
     
+    /*
+     * We need these global variables because the level loader doesn't allow
+     * sprites to access the game class. 
+     */
     public static EventManager eventManager;
+    public static ImageLoader imageLoader;
     
     public static void main (String[] args)
     {
@@ -25,6 +31,7 @@ public class Breakout extends VoogaGame
     public void initResources ()
     {
         eventManager = getEventManager();
+        imageLoader = getImageLoader();
         
         Paddle paddle = new Paddle(this, getWidth()/2, getHeight());
         getLevelManager().addPlayer(new SpriteGroup<Paddle>("paddle", paddle));
--- src/games/breakout/sprites/Block.java
+++ src/games/breakout/sprites/Block.java
@@ -1,5 +1,6 @@
 package games.breakout.sprites;
 
+import games.breakout.Breakout;
 import java.awt.image.BufferedImage;
 import vooga.resources.Direction;
 import vooga.sprites.improvedsprites.Sprite;
@@ -7,10 +8,31 @@ import vooga.sprites.improvedsprites.Sprite;
 @SuppressWarnings("serial")
 public class Block extends Sprite
 {
-    public Block(BufferedImage image, int x, int y, int s)
+    private int hp;
+    
+    public Block(BufferedImage image, int x, int y)
+    {
+        this(image, x, y, 0);
+    }
+    
+    public Block(BufferedImage image, int x, int y, int tile)
+    {
+        this(image, x, y, tile, 1);
+    }
+    
+    public Block(BufferedImage image, int x, int y, int tile, int durability)
     {
         super(image, x * image.getWidth(), y * image.getHeight());
         
+        setImage(Breakout.imageLoader.getImage("block", tile));
+        hp = durability;
+        
         this.setAngle(Direction.NORTH.getAngle());
+    }
+    
+    public void damage()
+    {
+        if (--hp == 0)
+            setActive(false);
     }
 }
