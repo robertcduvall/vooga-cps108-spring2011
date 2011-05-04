a2fa357ca91a928170d3f805892e0ff209fc1350
wbrown0389 <wbrown0389@5a3452c7-76ba-512d-50cd-69e64edf86dc>
2011-05-04 18:01:35 +0000

    Frogger! Or rather what will be a cheap knockoff

---
+++ trunk/src/games/frogger/Frogger.java
@@ -0,0 +1,22 @@
+package games.frogger;
+/**
+ * @author Wesley Brown
+ */
+
+import vooga.core.VoogaGame;
+
+public class Frogger extends VoogaGame {
+
+	@Override
+	public void updatePlayField(long elapsedTime) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void initResources() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
