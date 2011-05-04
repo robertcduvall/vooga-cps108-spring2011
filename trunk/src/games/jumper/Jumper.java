caf66f7b78d3ac979f5fd59f0d369326efb58723
chatcher9356@gmail.com <chatcher9356@gmail.com@5a3452c7-76ba-512d-50cd-69e64edf86dc>
2011-05-04 23:57:39 +0000

    Jumper: added game file and resources file to package

---
+++ trunk/src/games/jumper/Jumper.java
@@ -0,0 +1,43 @@
+package games.jumper;
+
+
+
+import java.awt.Dimension;
+
+import vooga.core.VoogaGame;
+import vooga.core.event.EventManager;
+import vooga.resources.images.ImageLoader;
+import vooga.sprites.spritebuilder.components.collisions.CollisionCircleC;
+import vooga.sprites.spritegroups.SpriteGroup;
+
+public class Jumper extends VoogaGame{
+
+	{distribute = true;}
+	
+	public static EventManager myEventManager;
+    public static ImageLoader myImageLoader;
+    //private Avatar myAvatar;
+    
+	public static void main(String[] args){
+		launchGame(new Jumper(), new Dimension(700,400),false);
+	}
+	@Override
+	public void updatePlayField(long elapsedTime) {
+		
+	}
+		
+
+	@Override
+	public void initResources() {
+		myEventManager = getEventManager();
+		myImageLoader = getImageLoader();
+		
+		//myAvatar = new Avatar(this);
+		//myAvatar.addComponent(new CollisionCircleC(myAvatar.getCenterPoint(), myAvatar.getWidth()/2));
+		//getLevelManager().addPlayer(new SpriteGroup<Avatar>("avatar", myAvatar));
+		getLevelManager().loadLevel(0);
+	
+	}
+
+}
+
