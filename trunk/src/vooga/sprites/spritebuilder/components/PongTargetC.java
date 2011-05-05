bb1ac8411bdb4e67dda12914323f7b146ea860ed
jsf.fan <jsf.fan@5a3452c7-76ba-512d-50cd-69e64edf86dc>
2011-05-05 08:30:28 +0000

    pong ai updated

--- trunk/src/vooga/sprites/spritebuilder/components/PongTargetC.java
+++ trunk/src/vooga/sprites/spritebuilder/components/PongTargetC.java
@@ -6,23 +6,30 @@ import vooga.sprites.spritebuilder.components.basic.TargetC;
 
 public class PongTargetC extends TargetC implements ISpriteUpdater
 {
+	private double AI_speed = 3.3;
 
     @Override
     public void update (Sprite s, long elapsedTime)
     {
         //s.setLocation(s.getX(), myTarget.getY());
     	
-    	int diff = (int)( myTarget.getY() - s.getCenterY());
-    	if(myTarget.getX() > 50)
-    		moveTowards(s, diff);
     	
-    }
+    	double middleOfScreen = s.getBackground().getHeight()/2;
+    	
+    	if(myTarget.getX() > 320)
+    		moveTowardsValue(s, myTarget.getY());
+    	else 
+			moveTowardsValue(s, middleOfScreen);
+    		
+    	
+    }    
 
-	private void moveTowards(Sprite s, int diff) {
+	private void moveTowardsValue(Sprite s, double desiredLocation) {
+		double diff = ( desiredLocation - s.getCenterY());
 		if(diff >= 0)
-    		s.setY(s.getY()+Math.min(4,diff));
+    		s.setY(s.getY()+Math.min(AI_speed,diff));
     	else
-    		s.setY(s.getY()+Math.max(-4,diff));
+    		s.setY(s.getY()+Math.max(-AI_speed,diff));
 		
 	}
 
