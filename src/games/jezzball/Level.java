3e8bf6759b668c29db904d4e60eecd94823ed0da
kevinwang29 <kevinwang29@gmail.com>
2011-05-05 00:44:33 +0000

    BUNDLE DOESN"T WORK!!!!!!!!!!!!!!!! SADNESS

--- src/games/jezzball/Level.java
+++ src/games/jezzball/Level.java
@@ -13,7 +13,9 @@ import java.util.Map;
 import vooga.core.VoogaGame;
 import vooga.core.event.IEventHandler;
 import vooga.levels.AbstractLevel;
+import vooga.levels.LevelManager;
 import vooga.reflection.Reflection;
+import vooga.resources.bundle.Bundle;
 import vooga.sprites.improvedsprites.Sprite;
 import vooga.sprites.spritegroups.SpriteGroup;
 
@@ -37,30 +39,43 @@ public class Level extends AbstractLevel
     private static int CHECKED = 5;
     
     private static int NUM_HIT_WALL=0;
+    private static int CONTACT_ON_BOTH_SIDES = 2;
     
     private Map<String, int[]> modifierMap;
-    private String[] mapKey= new String[]{"Up", "Down", "Right", "Left"};
-    private int[][] mapValue = new int[][]{new int[]{0,-20}, new int[]{0,20}, new int[]{20,0}, new int[]{-20,0}};
+    private String[] modifierMapKey= new String[]{"Up", "Down", "Right", "Left"};
+    private int[][] modifierMapValue = new int[][]{new int[]{0,-20}, new int[]{0,20}, new int[]{20,0}, new int[]{-20,0}};
     
-    int[][] tileArray = new int[15][15];
+    int[][] tileArray= new int[15][15]; // double array representing the grid layout of the game field
     
+    private Bundle resource;
     
     public Level (Collection<SpriteGroup<Sprite>> players, VoogaGame game)
     {
-        super(players, game);
+        super(players, game);        
         this.game = (Jezzball)game;
+        resource = getBundle();
+        //SPAWN_RATE = resource.getInteger("spawnRate");
+        //TOP_LEFT_CORNER = new Point(resource.getInteger("topLeftX"), resource.getInteger("topLeftY"));
+        
         initTileArray();
         addCollisionManager(new CreateWallCollision(game, getSpriteGroup("ball"), getSpriteGroup("tile")));
-        
-        modifierMap = new HashMap<String, int[]>();
-        for(int i=0; i<mapKey.length; i++){
-            modifierMap.put(mapKey[i], mapValue[i]);
-        }
-
+        initModifierMap();
         registerEvents();
         
     }
+    /**
+     * 
+     */
+    protected void initModifierMap() {
+        modifierMap = new HashMap<String, int[]>();
+        for(int i=0; i<modifierMapKey.length; i++){
+            modifierMap.put(modifierMapKey[i], modifierMapValue[i]);
+        }
+    }
     private void initTileArray() {
+        
+        //tileArray = new int[resource.getInteger("gridSizeX")][resource.getInteger("gridSizeY")];
+        
         for(int i = 0; i<tileArray.length; i++){
             for(int j = 0; j < tileArray.length; j++){
                 if(i==0|| i==tileArray.length-1|| j==0 || j==tileArray[i].length-1){
@@ -156,7 +171,7 @@ public class Level extends AbstractLevel
         }
         
         NUM_HIT_WALL++;
-        if(NUM_HIT_WALL==2){
+        if(NUM_HIT_WALL==CONTACT_ON_BOTH_SIDES){
             NUM_HIT_WALL=0;
             game.fireEvent(this,"Level.consolidateWall");
             setTileArray(TILE, WALL);
