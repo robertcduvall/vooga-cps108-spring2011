64e2b79dd0c6c20d298ac5945e982956279e195d
yhgoh88 <yhgoh88@gmail.com>
2011-05-05 08:34:47 +0000

    Moving the Arcade outside. 1!!!!!!!!!!!

---
+++ src/vooga/VoogaArcadeMain.java
@@ -0,0 +1,17 @@
+package vooga;
+import vooga.arcade.controller.ArcadeController;
+
+/**
+ * 
+ */
+public class VoogaArcadeMain
+{
+    
+	public static void main(String[] args)
+	{
+		ArcadeController start = new ArcadeController(
+				"VOOGA", "", 860, 730);
+		start.toString();
+		
+	}
+}
