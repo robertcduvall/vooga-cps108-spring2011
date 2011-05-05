7f5262ad8aa84d2f51e7270a6bc1a744e404fea3
sterling.dorminey <sterling.dorminey@gmail.com>
2011-05-05 00:58:17 +0000

    Moar keen, for those of you with a keen interest in this game.

---
+++ src/games/keen/collisions/TileCollision.java
@@ -0,0 +1,15 @@
+package games.keen.collisions;
+
+import games.keen.sprites.Keen;
+import games.keen.sprites.StaticTile;
+import vooga.collisions.collisionManager.BasicCollisionGroup;
+
+public class TileCollision extends BasicCollisionGroup<Keen, StaticTile> {
+	
+	@Override
+	public void collided(Keen s1, StaticTile s2) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
---
+++ src/games/keen/levels/Level.java
@@ -0,0 +1,40 @@
+package games.keen.levels;
+
+import java.util.Collection;
+
+import vooga.core.VoogaGame;
+import vooga.levels.AbstractLevel;
+import vooga.physics.forceGenerator.MassProportionalForceGenerator;
+import vooga.physics.util.Force;
+import vooga.physics.util.MassProportionalForce;
+import vooga.sprites.improvedsprites.Sprite;
+import vooga.sprites.spritegroups.SpriteGroup;
+
+public class Level extends AbstractLevel {
+
+	protected int offsetX, offsetY;
+	
+	public Level(Collection<SpriteGroup<Sprite>> players, VoogaGame game) {
+		super(players, game);
+		
+		offsetX = 0;
+		offsetY = 0;
+	}
+
+	@Override
+	public void loadLevel() {
+		double grav = super.getBundle().getDouble("gravity");
+		super.getPhysics().addGlobalForce(new MassProportionalForceGenerator(new Force(0, grav)), true);
+		super.addAllSpritesFromPool();
+		super.addBackground();
+	}
+
+	public int getOffsetX() {
+		return offsetX;
+	}
+	
+	public int getOffsetY() {
+		return offsetY;
+	}
+
+}
