02b81c9bcf75a7888be173b02ce467e8104da020
d.colon.s@gmail.com <d.colon.s@gmail.com@5a3452c7-76ba-512d-50cd-69e64edf86dc>
2011-05-05 07:57:46 +0000

    -refactoring stuff with stupid errors on it

--- trunk/src/vooga/gui/panes/TextInputPaneRefactored.java
+++ trunk/src/vooga/gui/panes/TextInputPaneRefactored.java
@@ -3,6 +3,7 @@ package vooga.gui.panes;
 import java.awt.Dimension;
 import java.awt.Graphics2D;
 import java.awt.image.BufferedImage;
+import java.util.Map;
 
 import com.golden.gamedev.gui.TTextField;
 import com.golden.gamedev.gui.toolkit.FrameWork;
@@ -21,12 +22,12 @@ public class TextInputPaneRefactored extends PopoverPane
     FrameWork textInputPaneFrame;
     TTextField tTextBox;
     VoogaButton acceptButton;
+    Map<TTextField, VoogaButton> textFieldsList;
    
     public TextInputPaneRefactored(PaneManager parent, BufferedImage acceptImage)
     {
         super(parent);
-        acceptButton = new VoogaButton(acceptImage, "Accept", new Dimension(50, 100));
-        
+        acceptButton = new VoogaButton(acceptImage, "Accept", new Dimension(50, 100));    
     }
    
     public void initResources()
@@ -42,5 +43,43 @@ public class TextInputPaneRefactored extends PopoverPane
         super.render(g);
         tTextBox.render(g);
         acceptButton.render(g);
+    }
+    
+    public void multipleRender(Graphics2D g)
+    {
+    	super.render(g);
+    	for(TTextField ttf : textFieldsList.keySet())
+    		ttf.render(g);
+    	for(VoogaButton vb : textFieldsList.values())
+    		vb.render(g);
+    }
+    
+    /**
+     * This method was added for the sake of adding multiple components as a list going down or across.
+     * @param x - x coordinate of the first text field
+     * @param y - y coordinate of the first text field
+     * @param dx - space between the next text field (x-coordinate) thats the same between all text fields
+     * @param dy - space between the next text field (y-coordinate) thats the same between all text fields
+     */
+    public void initMultipleResources(int x, int y, int dx, int dy)
+    {
+    	int deltaMultiplier = 1;
+    	textInputPaneFrame = new FrameWork(myParent.getParent().bsInput, myParent.getWidth(), myParent.getHeight());
+    	for(TTextField ttf : textFieldsList.keySet())
+    	{
+    		ttf = new TTextField("", myParent.getWidth()/2-50, myParent.getHeight()/2+20, x+(dx*deltaMultiplier), y+(dy*deltaMultiplier));
+    		textInputPaneFrame.add(ttf);
+    		deltaMultiplier++;
+    	}
+    }
+    
+    /**
+     * Adds another text field and accompanying button with it; most useful for the buttons
+     * @param textField - a Golden T TextField (see their GUI package information)
+     * @param VoogaButton - a GUI component button
+     */
+    public void setTextFieldandButton(TTextField textField, VoogaButton button)
+    {
+    	textFieldsList.put(textField, button);
     }
 }
