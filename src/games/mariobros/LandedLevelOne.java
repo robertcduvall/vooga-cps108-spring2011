f563b5798c46135344e425e4743cd253ccdf931a
yhgoh88 <yhgoh88@gmail.com>
2011-05-05 02:13:41 +0000

    IS IT MARIOBROS? OR IS IT LUNAR LANDER? MINDF*CK.

--- src/games/mariobros/LandedLevelOne.java
+++ src/games/mariobros/LandedLevelOne.java
@@ -2,10 +2,14 @@ package games.mariobros;
 
 import games.breakout.sprites.Ball;
 import games.breakout.sprites.Paddle;
+import games.mariobros.sprites.Lander;
+import vooga.core.event.EventManager;
 import vooga.levels.IGoal;
 import vooga.levels.LevelException;
 import vooga.levels.LevelManager;
 import vooga.levels.VoogaPlayField;
+import vooga.physics.forceGenerator.MassProportionalForceGenerator;
+import vooga.physics.util.Force;
 
 /**
  * A goal that checks if all the blocks on the playfield have
@@ -14,7 +18,7 @@ import vooga.levels.VoogaPlayField;
  * @author Misha
  *
  */
-public class LandedInRightArea implements IGoal
+public class LandedLevelOne implements IGoal
 {   
     private VoogaPlayField playfield;
     private LevelManager levels;
@@ -25,7 +29,8 @@ public class LandedInRightArea implements IGoal
     @Override
     public boolean checkCompletion (LevelManager levelManager)
     {
-        return false;
+    	 Lander l = (Lander) playfield.getSpriteGroup("lander").getActiveSprite();
+    	 return l.isSafe();
     }
 
 
@@ -36,24 +41,20 @@ public class LandedInRightArea implements IGoal
     public void progress ()
     {
         levels.updateNumOfLevelsCompleted();
-        
-        
-        Ball b = (Ball) playfield.getSpriteGroup("ball").getActiveSprite();
-        Paddle p = (Paddle) playfield.getSpriteGroup("paddle").getActiveSprite();
-        
-        if (b != null) b.setActive(false);
-        p.setBallCount(1 + p.getBallCount());
-        p.prepareNewBall();
+
+        Lander l = (Lander) playfield.getSpriteGroup("lander").getActiveSprite();
+        l.resetLander();
         
         try
         {
             levels.loadNextLevel();
+           
         }
-        catch (LevelException e)
+        catch (Exception e)
         {
-            /* TODO win the game better */
             System.out.println("You win!");
-            System.exit(0);  
+            levels.getCurrentGame().finish();
+            
         }
     }
 
--- src/games/mariobros/MarioBros.java
+++ src/games/mariobros/MarioBros.java
@@ -3,45 +3,52 @@ package games.mariobros;
 import games.mariobros.sprites.Lander;
 
 import java.awt.Dimension;
+
 import vooga.core.VoogaGame;
 import vooga.core.event.EventManager;
+import vooga.core.event.IEventHandler;
 import vooga.resources.images.ImageLoader;
-import vooga.sprites.spritebuilder.components.collisions.CollisionCircleC;
 import vooga.sprites.spritegroups.SpriteGroup;
 
-/**
- * Breakout. Bounce a ball off a paddle to destroy bricks.
- * 
- * @author Misha
- *
- */
 public class MarioBros extends VoogaGame
 {
-    {distribute = true;}
-    
-    /*
-     * We need these global variables because the level loader doesn't allow
-     * sprites to access the game class. 
-     */
-    public static EventManager eventManager;
-    public static ImageLoader imageLoader;
-    
-    public static void main (String[] args)
-    {
-        launchGame(new MarioBros(), new Dimension(640, 480), false);
-    }
+	public static EventManager eventManager;
+	public static ImageLoader imageLoader;
 
-    @Override
-    public void updatePlayField (long elapsedTime) {}
+	public static void main(String[] args)
+	{
+		launchGame(new MarioBros(), new Dimension(640, 480), false);
+	}
 
-    @Override
-    public void initResources ()
-    {
-        eventManager = getEventManager();
-        imageLoader = getImageLoader();
-        
-        Lander mario = new Lander(this, getWidth()/2, getHeight()/8);
-        getLevelManager().addPlayer(new SpriteGroup<Lander>("lander", mario));
-        getLevelManager().loadLevel(0);
-    }
+	@Override
+	public void updatePlayField(long elapsedTime)
+	{
+	}
+
+	@Override
+	public void initResources()
+	{
+		eventManager = getEventManager();
+		imageLoader = getImageLoader();
+
+		eventManager.registerEventHandler("Game.Close", new IEventHandler()
+		{
+			@Override
+			public void handleEvent(Object o)
+			{
+				closeGame();
+			}
+		});
+		
+		Lander mario = new Lander(this, getWidth() / 2, getHeight() / 8);
+		getLevelManager().addPlayer(new SpriteGroup<Lander>("lander", mario));
+		getLevelManager().loadLevel(0);
+
+	}
+
+	public void closeGame()
+	{
+		this.finish();
+		this.stop();
+	}
 }
