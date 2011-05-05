964d2908cb26988d7228100bcf1179908ee52d6f
chatcher9356@gmail.com <chatcher9356@gmail.com@5a3452c7-76ba-512d-50cd-69e64edf86dc>
2011-05-05 02:05:38 +0000

    Jumper: Added collision class

---
+++ trunk/src/games/jumper/collisions/AvatarToBlock.java
@@ -0,0 +1,18 @@
+package games.jumper.collisions;
+
+import games.jumper.sprites.Avatar;
+import games.jumper.sprites.Block;
+import games.jumper.sprites.Door;
+import vooga.collisions.collisionManager.BasicCollisionGroup;
+
+public class AvatarToBlock extends BasicCollisionGroup<Avatar, Block >{
+
+	@Override
+	public void collided(Avatar avatar, Block block) {
+		
+	}
+
+
+	
+	
+}
