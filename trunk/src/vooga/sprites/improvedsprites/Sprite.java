34a9242ab60212c344e2709304e51b2eba500f51
jgenk90 <jgenk90@5a3452c7-76ba-512d-50cd-69e64edf86dc>
2011-05-05 01:07:09 +0000

    minor arbitrary rotation fix

--- trunk/src/vooga/sprites/improvedsprites/Sprite.java
+++ trunk/src/vooga/sprites/improvedsprites/Sprite.java
@@ -522,7 +522,7 @@ public class Sprite extends BaseSprite
     	AffineTransform aTransform = new AffineTransform();
         aTransform.translate((int) this.getX() +width/2, 
                              (int) this.getY()+height/2);
-        aTransform.rotate(Math.toRadians(this.getAngle()+90));
+        aTransform.rotate(Math.toRadians(this.getAngle()+getArbitraryRotate()));
         aTransform.translate((int) -width/2, 
                              (int) -height/2);
         g.drawImage(image.getScaledInstance(width, height, 0),aTransform,null);
@@ -530,6 +530,11 @@ public class Sprite extends BaseSprite
     }
 
 
+
+
+	private Double getArbitraryRotate() {
+		return 90.0;
+	}
 
 
 	protected void renderComponents (Graphics2D g, int x, int y)
