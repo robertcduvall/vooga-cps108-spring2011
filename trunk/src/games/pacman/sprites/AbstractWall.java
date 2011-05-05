cb6410e3d09d7e30b6478fff9d9f0c13c7fb1b63
daniel.sharkey1 <daniel.sharkey1@5a3452c7-76ba-512d-50cd-69e64edf86dc>
2011-05-05 01:53:20 +0000

    Using built in 90 degree override

--- trunk/src/games/pacman/sprites/AbstractWall.java
+++ trunk/src/games/pacman/sprites/AbstractWall.java
@@ -12,19 +12,14 @@ public abstract class AbstractWall extends Sprite{
 
 	public AbstractWall(BufferedImage image, int x, int y) {
 		super(image,x,y);
-        this.setAngle(0);
 	}
 	
+
 	@Override
-	public void render(Graphics2D g,int x,int y) {
-		AffineTransform aTransform = new AffineTransform();
-        aTransform.translate((int) this.getX() +width/2, 
-                             (int) this.getY()+height/2);
-        aTransform.rotate(Math.toRadians(this.getAngle()));
-        aTransform.translate((int) -width/2, 
-                             (int) -height/2);
-        g.drawImage(ImageUtil.resize(image, width, height),aTransform,null);
-        super.renderComponents(g, x, y);
+	public Double getArbitraryRotate() {
+		return 0D;
 	}
 
+	
+	
 }
