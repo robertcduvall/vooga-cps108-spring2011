12cc96ae3a7fc7f15b61aef908413e2bdba9b1ac
yhgoh88@gmail.com <yhgoh88@gmail.com@5a3452c7-76ba-512d-50cd-69e64edf86dc>
2011-05-05 13:44:09 +0000

    Seriously....... branch your own version if you want to use this. It's rather rude.

--- trunk/src/games/tetris/src/Context/ITetrisContext.java
+++ trunk/src/games/tetris/src/Context/ITetrisContext.java
@@ -1,22 +1,35 @@
-package games.lolcats.src.Context;
+package games.tetris.src.Context;
 
-import games.lolcats.src.Minos.Tetromino;
+import games.tetris.src.Main.TetrisMatrix;
+import games.tetris.src.TetrisComponents.ActiveMino;
 
-public interface ITetrisContext {
-	Tetromino getActiveMino();
+import com.golden.gamedev.object.Timer;
 
-	// TetrisMatrix getTetrisMatrix();
-	// Timer getDasTimer();
-	// Timer getArrTimer();
-	// Timer getBlockLockTimer();
-	// Timer getSoftDropTimer();
-	// void subtractBlockLockDelayTotal(int time);
-	// void resetBlockLockDelayTotal();
-	// void generateNextBlock();
-	// void adjustDasBy(int i);
-	void finish();
+public interface ITetrisContext
+{
+	ActiveMino getActiveMino();
+
+	TetrisMatrix getTetrisMatrix();
+
+	Timer getDasTimer();
+
+	Timer getArrTimer();
+
+	Timer getBlockLockTimer();
+	
+	Timer getSoftDropTimer();
+
+	void subtractBlockLockDelayTotal(int time);
+	
+	void resetBlockLockDelayTotal();
+	
+	void generateNextBlock();
+
+	void adjustDasBy(int i);
 
 	void reset();
-	// boolean holdMino();
-	// void setHoldUsed(boolean b);
+
+	boolean holdMino();
+
+	void setHoldUsed(boolean b);
 }
