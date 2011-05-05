864ef006d8e6ed3c15314c4344acc7f32e96129d
moongwiff <moongwiff@gmail.com>
2011-05-05 05:23:33 +0000

    PHYSICS: Fixed a math error

--- src/vooga/physics/tests/TestNewtonianBehavior.java
+++ src/vooga/physics/tests/TestNewtonianBehavior.java
@@ -8,7 +8,6 @@ import java.util.Collection;
 
 import org.junit.Test;
 
-import vooga.physics.collisionBehavior.EmptyCollisionBehavior;
 import vooga.physics.forceGenerator.AbstractForceGenerator;
 import vooga.physics.forceGenerator.BasicForceGenerator;
 import vooga.physics.util.Force;
@@ -46,18 +45,13 @@ public class TestNewtonianBehavior {
     }
 
     @Test
-    /*
-     * So applyCollision isn't getting overridden...
-     */
     public void testPhysicalCollision(){
-        BasicPhysicsC basic1 = new BasicPhysicsC(new Velocity(10,new Angle(0)), 1);
+        BasicPhysicsC basic1 = new BasicPhysicsC(new Velocity(5,new Angle(0)), 1);
         BasicPhysicsC basic2 = new BasicPhysicsC(new Velocity(5,new Angle(Math.PI)), 1);
         Angle collisionAngle = new Angle(Math.PI/2);
-        EmptyCollisionBehavior behave2 = basic2.getCollisionBehavior();
-        basic1.applyCollision(behave2, collisionAngle, new Point(0,0), 1);
+        basic1.applyCollision(basic2.getCollisionBehavior(), collisionAngle, new Point(0,0), 1);
         basic2.applyCollision(basic1.getCollisionBehavior(), collisionAngle, new Point(0,0), 1);
         Velocity delta1 = basic1.getDeltaVelocity();
-        assertEquals("Delta1 magnitude", 5, delta1.getMagnitude(),0);
-        assertEquals("Delta1 angle", Math.PI, delta1.getAngle().getRadians(),0);
+        assertEquals("Delta1 magnitude", 10, delta1.getMagnitude(),0);
     }
 }
