7e54bb8d279ef99d4c63f06800a8bb7df6015a6e
CJAMES1224 <CJAMES1224@5a3452c7-76ba-512d-50cd-69e64edf86dc>
2011-05-05 13:14:37 +0000

    Comments and a small changes. Last commit I ever plan on doing.

--- trunk/src/vooga/replay/stateTableRefactored/State.java
+++ trunk/src/vooga/replay/stateTableRefactored/State.java
@@ -9,6 +9,17 @@ import vooga.replay.SpriteReplayData;
 import vooga.sprites.improvedsprites.Sprite;
 import vooga.sprites.spritegroups.SpriteGroup;
 
+/**
+ * 
+ * Class that represents a "State in time" - A Map of Sprites to their ReplayData,
+ * and the methods that are added here for ease.
+ * 
+ * @author CJAMES
+ * 
+ * For refactor project
+ *
+ */
+
 @SuppressWarnings("serial")
 public class State implements Serializable{
 	
@@ -18,6 +29,10 @@ public class State implements Serializable{
 		stateMap = new HashMap<Sprite, SpriteReplayData>();
 	}
 	
+	/**
+	 * Adds Sprites from sprite group to the current State.
+	 * @param s Sprite group from which sprites will be added to state
+	 */
 	public void addSpriteGroupToState(SpriteGroup<Sprite> s){
 		for (Sprite sprite : s.getSprites()) {
 			if (sprite != null && sprite.isActive()) {
@@ -25,13 +40,20 @@ public class State implements Serializable{
 			}
 		}
 	}
-	
+	/**
+	 * Renders all the sprites in this state.
+	 * @param g Graphics2D
+	 */
 	public void render(Graphics2D g){
 		for (Sprite sprite : stateMap.keySet()) {
 			sprite.render(g);
 		}
 	}
 
+	/**
+	 * Changes each sprite being updated to match the data in their 
+	 * SpriteReplayData, as found in the stateMap
+	 */
 	public void transformSpritesToState() {
 		for (Sprite sprite : stateMap.keySet()) {
 			sprite = stateMap.get(sprite).transformSprite(sprite);
