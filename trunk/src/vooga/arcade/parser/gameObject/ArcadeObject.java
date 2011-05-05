18bc96e297ac7af581a68cf5968ffe966d3d3417
yhgoh88@gmail.com <yhgoh88@gmail.com@5a3452c7-76ba-512d-50cd-69e64edf86dc>
2011-05-05 05:42:43 +0000

    Defaulting an Image so that it wont null pointer and crash the arcade.

--- trunk/src/vooga/arcade/parser/gameObject/ArcadeObject.java
+++ trunk/src/vooga/arcade/parser/gameObject/ArcadeObject.java
@@ -7,26 +7,26 @@ import java.io.IOException;
 import javax.imageio.ImageIO;
 
 import org.jdom.Element;
+
 /**
  * 
- * General object used to contain information that can be
- * displayed in the arcade.
+ * General object used to contain information that can be displayed in the
+ * arcade.
  * 
  * @author Yong-Hui Goh
  * @author KevinWang
- *
+ * 
  */
 public abstract class ArcadeObject
 {
 	protected BufferedImage image;
 	protected Element root;
 	protected String path;
-	
-	
+
 	public ArcadeObject(Element root, String path)
 	{
-	    this.root = root;
-	    this.path = path;
+		this.root = root;
+		this.path = path;
 	}
 
 	/**
@@ -34,20 +34,33 @@ public abstract class ArcadeObject
 	 * 
 	 * @return the image
 	 */
-	public BufferedImage getImage() {
-        if(image!= null) return image;
-//        System.out.println(root.getChildText("image"));
-        try {
-            image = ImageIO.read(new File(root.getChildText("image")));
-        }
-        catch (IOException e) {
-            e.printStackTrace();
-        }
-        return image;
-    }
+	public BufferedImage getImage()
+	{
+		if (image != null)
+			return image;
+		// System.out.println(root.getChildText("image"));
+		try
+		{
+			image = ImageIO.read(new File(root.getChildText("image")));
+		}
+		catch (IOException e)
+		{
+			e.printStackTrace();
+			try
+			{
+				image = ImageIO.read(new File("src/games/tetris/kea.jpg"));
+			}
+			catch (IOException e1)
+			{
+
+			}
+		}
+		return image;
+	}
 
 	/**
 	 * return name of the object
+	 * 
 	 * @return the name
 	 */
 	public String getName()
