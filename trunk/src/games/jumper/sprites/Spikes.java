ffec8f15db1a9a5bd463da27d96917548fd1d190
chatcher9356@gmail.com <chatcher9356@gmail.com@5a3452c7-76ba-512d-50cd-69e64edf86dc>
2011-05-05 02:04:58 +0000

    Jumper: fixing a bug with collisions

--- trunk/src/games/jumper/sprites/Spikes.java
+++ trunk/src/games/jumper/sprites/Spikes.java
@@ -1,6 +1,5 @@
 package games.jumper.sprites;
 
-import java.awt.Graphics2D;
 import java.awt.image.BufferedImage;
 
 import vooga.resources.Direction;
@@ -9,11 +8,7 @@ import vooga.sprites.improvedsprites.Sprite;
 @SuppressWarnings("serial")
 public class Spikes extends Sprite{
 	
-	@Override
-    public void render(Graphics2D g) {
-            super.render(g);
-            this.getCollisionShape().render(g);
-    }
+
 	public Spikes(BufferedImage image, int x, int y){
 		this.setImage(image);
 		this.setX(x);
