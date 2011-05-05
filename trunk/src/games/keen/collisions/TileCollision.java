f8ba19049b649caf0b6f321ed815f57e187e49ac
ncklug <ncklug@5a3452c7-76ba-512d-50cd-69e64edf86dc>
2011-05-05 01:26:00 +0000

    slight repulsion on collision impact in keen

--- trunk/src/games/keen/collisions/TileCollision.java
+++ trunk/src/games/keen/collisions/TileCollision.java
@@ -1,15 +1,18 @@
 package games.keen.collisions;
 
+import java.awt.Point;
 import games.keen.sprites.Keen;
 import games.keen.sprites.StaticTile;
 import vooga.collisions.collisionManager.BasicCollisionGroup;
+import vooga.physics.VoogaPhysicsMediator;
+import vooga.util.math.Angle;
 
 public class TileCollision extends BasicCollisionGroup<Keen, StaticTile> {
 	
 	@Override
 	public void collided(Keen s1, StaticTile s2) {
 		// TODO Auto-generated method stub
-		
+	    s1.setVerticalSpeed(-0.01);
 	}
 
 }
