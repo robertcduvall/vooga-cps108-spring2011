a3eec2d1ca73e4280ea7749043c27a2d59b81553
chatcher9356 <chatcher9356@gmail.com>
2011-05-05 00:06:49 +0000

    Jumper: Added Collisions(NEED HELP) and uncommented jumper.java

---
+++ src/games/jumper/collisions/AvatarToDoor.java
@@ -0,0 +1,14 @@
+package games.jumper.collisions;
+
+import games.jumper.sprites.Avatar;
+import games.jumper.sprites.Door;
+import vooga.collisions.collisionManager.BasicCollisionGroup;
+
+public class AvatarToDoor extends BasicCollisionGroup<Avatar, Door >{
+
+	@Override
+	public void collided(Avatar s1, Door s2) {
+		s2.setActive(false);
+	}
+
+}
---
+++ src/games/jumper/collisions/AvatarToSpikes.java
@@ -0,0 +1,24 @@
+package games.jumper.collisions;
+
+import games.jumper.Jumper;
+import games.jumper.sprites.Avatar;
+import games.jumper.sprites.Spikes;
+import vooga.collisions.collisionManager.BasicCollisionGroup;
+
+public class AvatarToSpikes extends BasicCollisionGroup<Avatar,Spikes>{
+
+	/**public AvatarToSpikes(){
+		super();
+		
+	}
+	public AvatarToSpikes(SpriteGroup<Avatar> s1, SpriteGroup<Spikes> s2){
+		super(s1,s2);
+		System.out.println("hello");
+	}*/
+	@Override
+	public void collided(Avatar s1, Spikes s2) {
+		s1.setActive(false);
+		Jumper.myEventManager.fireEvent(this, "Game.Avatar.Died");
+	}
+
+}
