0a5499509a1cf5960498711c1cb8705a4cdba98a
dmiller309@gmail.com <dmiller309@gmail.com@5a3452c7-76ba-512d-50cd-69e64edf86dc>
2011-05-05 13:11:52 +0000

    commit.

---
+++ trunk/src/games/tetris/src/Minos/Tetromino.java
@@ -0,0 +1,29 @@
+package games.lolcats.src.Minos;
+
+import games.lolcats.src.Context.ITetrisContext;
+
+import java.awt.image.BufferedImage;
+
+import com.golden.gamedev.object.Sprite;
+
+public class Tetromino extends Sprite {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 4913020634810530622L;
+	private final double speed = 0.02;
+
+	public Tetromino(ITetrisContext tc, BufferedImage image) {
+		super(image);
+	}
+
+	public static final double LEFT = Math.PI;
+	public static final double RIGHT = 0;
+	public static final double UP = Math.PI * 3D / 2D;
+	public static final double DOWN = Math.PI / 2D;
+
+	public void setDirection(double angle) {
+		this.setVerticalSpeed(Math.sin(angle) * speed);
+		this.setHorizontalSpeed(Math.cos(angle) * speed);
+	}
+}
