f1e66b40a5ac682e5e10ebbf7ee56f233d7e15fe
jgenk90 <jgenk90@5a3452c7-76ba-512d-50cd-69e64edf86dc>
2011-05-05 00:40:17 +0000

    added some images to arrow, gogo 1000 commits

--- src/games/asteroids/Asteroids.java
+++ src/games/asteroids/Asteroids.java
@@ -33,7 +33,7 @@ public class Asteroids extends VoogaGame
         myShip = new Ship(this);
         myShip.setX(150);
         myShip.setY(150);
-        myShip.addComponent(new CollisionCircleC(myShip.getCenterPoint(), myShip.getWidth()/2));
+        myShip.addComponent(new CollisionCircleC(myShip.getCenterPoint(), myShip.getHeight()/2));
         getLevelManager().addPlayer(new SpriteGroup<Ship>("ship", myShip));
         
         // TODO getResourceManager().getKeyMap().registerEventHandler(this);
