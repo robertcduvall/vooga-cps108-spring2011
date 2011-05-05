617ef088b74ee69449f727a8a1d625cd306b59dd
chatcher9356@gmail.com <chatcher9356@gmail.com@5a3452c7-76ba-512d-50cd-69e64edf86dc>
2011-05-05 11:04:30 +0000

    LevelEditor:Removed some stuff

--- trunk/src/vooga/leveleditor/gui/Viewport.java
+++ trunk/src/vooga/leveleditor/gui/Viewport.java
@@ -90,7 +90,6 @@ public class Viewport extends JLayeredPane
          */
         @Override
         public void mouseClicked(MouseEvent arg0) {
-        	System.out.println(mySprites.size());
             for(int i=0; i<mySprites.size();i++){
                 if(!arg0.isControlDown()){
                     if(mySprites.get(i).moveIfSelected(x, y));
@@ -100,35 +99,8 @@ public class Viewport extends JLayeredPane
                 }
             }
         }
-
-        /**
-         * Brings up a new internal frame that serves as a popup to allow the user to
-         * edit the sprites properties in a text file.
-         * 
-         * Currently unused
-         */
-        /*
-		private void editSelectedSprite() {
-			for(int i=0; i<mySprites.size();i++){
-				mySprites.get(i).setSpriteProperties(x, y);
-			}
-		}
-         */
-        /*
-         * FIXME: iterate through the for loop backwards not sure if this is needed
-         * anymore
-         * 
-         * currently unused
-         */
-        /*
-		private void moveSelectedSprite() {
-			if(mySprites.size()>0){
-				for(DraggableImage i: mySprites){
-					if(i.moveIfSelected(x, y));
-				}
-			}
-		}
-         */
+        
+         
         /**
          * the rest of these methods are unneeded, but come standard with the
          * MouseListener interface
