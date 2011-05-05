a74dad07e1fdf9a6d326d898177befb7a04caaa5
jgenk90 <jgenk90@5a3452c7-76ba-512d-50cd-69e64edf86dc>
2011-05-05 16:02:04 +0000

    random shapes generating

--- src/games/arrow/Level.java
+++ src/games/arrow/Level.java
@@ -1,15 +1,23 @@
 package games.arrow;
 
+import games.arrow.sprites.Arrow;
 import games.arrow.sprites.Enemy;
 
 import java.util.Collection;
 
 import com.golden.gamedev.util.ImageUtil;
 
+import vooga.collisions.shapes.ShapeFactory;
+import vooga.collisions.shapes.regularShapes.Polygon;
 import vooga.core.VoogaGame;
 import vooga.core.event.IEventHandler;
 import vooga.levels.AbstractLevel;
+import vooga.physics.forceGenerator.MassProportionalForceGenerator;
+import vooga.physics.util.Force;
 import vooga.sprites.improvedsprites.Sprite;
+import vooga.sprites.spritebuilder.builder.SpriteBuilder;
+import vooga.sprites.spritebuilder.components.basic.PermAccelerationC;
+import vooga.sprites.spritebuilder.components.collisions.CollisionPolygonC;
 import vooga.sprites.spritegroups.SpriteGroup;
 
 /**
@@ -21,9 +29,12 @@ public class Level extends AbstractLevel
 {
 	
 	public VoogaGame myGame;
+	private SpriteBuilder<Enemy> EnemyBuilder;
     public Level (Collection<SpriteGroup<Sprite>> players, VoogaGame game)
     {
         super(players, game);
+        EnemyBuilder = new SpriteBuilder<Enemy>(CollisionPolygonC.class);
+        this.getPhysics().addGlobalForce(new MassProportionalForceGenerator(new Force(0, -.0005)),true);
         myGame = game;
         game.registerEventHandler("EnemySpawn", new IEventHandler()
         {
@@ -34,7 +45,7 @@ public class Level extends AbstractLevel
                 }
         });
         
-        game.addPeriodicTimer("OneTime", 2000, "EnemySpawn");
+        game.addPeriodicTimer("OneTime", 1500, "EnemySpawn");
     }
 
     @Override
@@ -45,7 +56,9 @@ public class Level extends AbstractLevel
     }
     
     protected void spawnEnemy() {
-    	Sprite enemy = myGame.getLevelManager().addArchetypeSprite("enemy" + (int)Math.floor((Math.random()*5)), (int)(getBackground().getWidth()*Math.random()),(int)((getBackground().getHeight()/2)*Math.random()));
+    	Enemy enemy = (Enemy) myGame.getLevelManager().addArchetypeSprite("enemy" + (int)Math.floor((Math.random()*5)), (int)(getBackground().getWidth()*Math.random()),(int)((getBackground().getHeight()/2)*Math.random())); 
+    	int n =  (int) Math.ceil((Math.random()*20)+1);
+    	enemy = EnemyBuilder.buildSprite(enemy, new Polygon(enemy.getCenterX(), enemy.getCenterY(),n, enemy.getWidth()/2));
     	enemy.setBackground(getBackground());
 		getSpriteGroup("enemy").addSprites(enemy);
 	}
