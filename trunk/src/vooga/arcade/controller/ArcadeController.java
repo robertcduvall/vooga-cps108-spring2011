35f59cdf9ecff142516eac865006f8e2a36643fe
ascripa@gmail.com <ascripa@gmail.com@5a3452c7-76ba-512d-50cd-69e64edf86dc>
2011-05-06 21:12:40 +0000

    updated arcade controller

--- trunk/src/vooga/arcade/controller/ArcadeController.java
+++ trunk/src/vooga/arcade/controller/ArcadeController.java
@@ -3,18 +3,18 @@ package vooga.arcade.controller;
 import java.awt.Dimension;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
-import java.util.ArrayList;
+//import java.util.ArrayList;
 import java.util.List;
-
-import javax.swing.JPanel;
+//import javax.swing.JPanel;
 import javax.swing.JToolBar;
 import vooga.arcade.model.ArcadeModel;
 import vooga.arcade.parser.ArcadeGameObject;
-import vooga.arcade.parser.ArcadeUserObject;
-import vooga.arcade.parser.gameObject.ArcadeObject;
+//import vooga.arcade.parser.ArcadeUserObject;
+//import vooga.arcade.parser.gameObject.ArcadeObject;
 import vooga.arcade.view.buttonToolBar.ButtonBar;
+//import vooga.arcade.view.gui.ProductionState;
 import vooga.arcade.view.gui.VoogaViewer;
-import vooga.user.controller.LoginController;
+//import vooga.user.controller.LoginController;
 import vooga.user.voogauser.VoogaUser;
 
 
@@ -31,6 +31,9 @@ public class ArcadeController
     private ArcadeModel model;
     private VoogaUser currentUser;
 
+
+    //private ProductionState prodSt;
+
     public ArcadeController (String string, String string2, int i, int j)
     {
         model = new ArcadeModel();
@@ -41,27 +44,32 @@ public class ArcadeController
     public ArcadeController ()
     {}
 
+
     public void displayError (String s)
     {
         view.showError(s);
     }
 
+
     public List<ArcadeGameObject> queryModel (String tag, String[] query)
     {
-    	model.filterArcadeGameList(tag, query);
+        model.filterArcadeGameList(tag, query);
         return model.getCurrentGameList();
     }
-   
+
+
     /**
-     * Uses reflection to call the correct sorting algorithm in the model based on what was selected in 
-     * the drop-down menu.
+     * Uses reflection to call the correct sorting algorithm in the model based
+     * on what was selected in the drop-down menu.
      */
     @SuppressWarnings("unchecked")
     public void sortInModel (String sort)
         throws SecurityException,
             NoSuchMethodException,
             ClassNotFoundException,
-            IllegalArgumentException, IllegalAccessException, InvocationTargetException
+            IllegalArgumentException,
+            IllegalAccessException,
+            InvocationTargetException
     {
 
         Class<?> thisClass = Class.forName("ArcadeController");
@@ -70,32 +78,32 @@ public class ArcadeController
         sortBy.invoke(new ArcadeController(), parameters);
     }
 
-    public void swapToolbarButton(VoogaUser user)
-    {
-        String name;
-        currentUser = user;
-        if (currentUser==null)
-            name = "Login";
-        else name = user.getUsername();
-        
-        ButtonBar.swapButtons(name);
-    }
-
-    public VoogaUser getUser()
+    public VoogaUser getUser ()
     {
         return currentUser;
     }
-   
-    public void updateGameObjects(List<ArcadeGameObject> arcadeGameList, String searchQuery){
+
+
+    public void updateGameObjects (List<ArcadeGameObject> arcadeGameList,
+                                   String searchQuery)
+    {
         view.updateGameObjectList(arcadeGameList, searchQuery);
     }
-    
-    public void updateOutlineText(String s, int index){
+
+
+    public void updateOutlineText (String s, int index)
+    {
         view.getMiddleFrame().getTextPanes().changePaneToURL(index, s);
     }
-    
-    public void updateButtonBar(JToolBar newBar)
+
+
+    public void updateButtonBar(VoogaUser newUser)
     {
+        currentUser = newUser;
+        JToolBar newBar =
+            ButtonBar.createButtonToolBar(this,
+                                          view.ps.getStateResources(),
+                                          view.ps.currentState);
         view.switchToolBar(newBar);
     }
 }
